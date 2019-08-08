7d57c766b98ae67399152248d3086f7cce3187ed
Rizal Alfiannor <rizalblue@gmail.com>
2019-08-08 21:06:27 +0700

    delete folder vendor in git

--- config/database.php
+++ config/database.php
@@ -47,11 +47,12 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_general_ci',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+            'modes'  => [],
         ],
 
         'pgsql' => [

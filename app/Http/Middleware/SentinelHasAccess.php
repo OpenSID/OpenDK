6ac10560345f32e279200fbe1efe5852fcce3397
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-11-04 11:41:20 +0700

    [2.x] - Fix load facade class

--- app/Http/Middleware/SentinelHasAccess.php
+++ app/Http/Middleware/SentinelHasAccess.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Sentinel;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 
 use function abort;
 use function flash;

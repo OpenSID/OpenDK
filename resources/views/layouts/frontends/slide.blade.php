361debbe02a46eb579497732cf45bea8932cee89
dirgareborn <kikifatiroh@gmail.com>
2020-12-09 16:26:21 +0800

    Template Halaman Pengunjung

---
+++ resources/views/layouts/frontends/slide.blade.php
@@ -0,0 +1,34 @@
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+      </ol>
+      <div class="carousel-inner" role="listbox" style=" width:100%; max-height: 450px !important;">
+        <div class="item active">
+          <img src="{{ asset('slide/bg_slide1.jpg') }}" alt="First slide" width="100%">
+          <div class="carousel-caption">
+            First Slide
+          </div>
+        </div>
+        <div class="item">
+          <img src="{{ asset('slide/bg_slide1.jpg') }}" alt="First slide" width="100%">
+          <div class="carousel-caption">
+            Second Slide
+          </div>
+        </div>
+        <div class="item">
+          <img src="{{ asset('slide/bg_slide1.jpg') }}" alt="First slide" width="100%">
+          <div class="carousel-caption">
+            Third Slide
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="fa fa-angle-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="fa fa-angle-right"></span>
+      </a>
+    </div>
+<div class="clearfix"></div>

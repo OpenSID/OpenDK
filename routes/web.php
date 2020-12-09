f6ccf54dd0d1652e2ba68bb2d9994dcca91791d8
dirgareborn <kikifatiroh@gmail.com>
2020-12-09 19:41:29 +0800

    modified route

--- routes/web.php
+++ routes/web.php
@@ -577,7 +577,100 @@ Route::group(['middleware' => 'installed'], function () {
     });
 
 
-});
+    
+    Route::get('/sitemap', 'SitemapController@index');
+    Route::get('/sitemap/prosedur', 'SitemapController@prosedur');
 
-Route::get('/sitemap', 'SitemapController@index');
-Route::get('/sitemap/prosedur', 'SitemapController@prosedur');
+      /**
+     *
+     * Grouep Routing API Internal for Select2
+     */
+    
+    //Users JSON
+    Route::get('/api/users', function () {
+        return \App\Models\User::where('first_name', 'LIKE', '%' . request('q') . '%')->paginate(10);
+    });
+    
+    // All Provinsi Select2
+    Route::get('/api/provinsi', function () {
+        return \App\Models\Wilayah::whereRaw('LENGTH(kode) = 2')->where('nama', 'LIKE', '%' . strtoupper(request('q')) . '%')->paginate(10);
+    });
+    
+    // All Kabupaten Select2
+    Route::get('/api/kabupaten', function () {
+        return \App\Models\Wilayah::whereRaw('LENGTH(kode) = 5')->where('nama', 'LIKE', '%' . strtoupper(request('q')) . '%')->paginate(10);
+    });
+    
+    //  All Kecamatan Select2
+    Route::get('/api/kecamatan', function () {
+        return \App\Models\Wilayah::whereRaw('LENGTH(kode) = 8')->where('nama', 'LIKE', '%' . strtoupper(request('q')) . '%')->paginate(10);
+    });
+    
+    // All Desa Select2
+    Route::get('/api/desa', function () {
+        return \App\Models\Wilayah::whereRaw('LENGTH(kode) = 13')->where('nama', 'LIKE', '%' . strtoupper(request('q')) . '%')->paginate(10);
+    });
+    
+    // Desa Select2 By Kecamatan ID
+    Route::get('/api/desa-by-kid', function () {
+        return DB::table('ref_desa')->select('kode', 'nama')->whereRaw('LENGTH(kode) = 2')->where('kecamatan_id', '=', strtoupper(request('kid')))->get();
+    })->name('api.desa-by-kid');
+    
+    // All Profil Select2
+    Route::get('/api/profil', function () {
+        return DB::table('das_profil')
+            ->join('ref_wilayah', 'das_profil.kecamatan_id', '=', 'ref_wilayah.kode')
+            ->select('ref_wilayah.kode', 'ref_kecamatan.nama')
+            ->where('ref_wilayah.nama', 'LIKE', '%' . strtoupper(request('q')) . '%')
+            ->paginate(10);
+    })->name('api.profil');
+    
+    // Profil By id
+    Route::get('/api/profil-byid', function () {
+        return DB::table('das_profil')
+            ->join('ref_kecamatan', 'das_profil.kecamatan_id', '=', 'ref_kecamatan.id')
+            ->select('ref_kecamatan.id', 'ref_kecamatan.nama')
+            ->where('ref_kecamatan.id', '=', request('id'))->get();
+    })->name('api.profil-byid');
+    
+    // All Penduduk Select2
+    Route::get('/api/penduduk', function () {
+        return \App\Models\Penduduk::where('nama', 'LIKE', '%' . strtoupper(request('q')) . '%')->paginate(10);
+    })->name('api.penduduk');
+    
+    // Penduduk By id
+    Route::get('/api/penduduk-byid', function () {
+        return DB::table('das_penduduk')
+            ->where('id', '=', request('id'))->get();
+    })->name('api.penduduk-byid');
+
+    Route::get('/api/test', function () {
+        $return = [];
+        $a = ['year' => 2018];
+        $return = array_merge($return, $a);
+        $b = ['penyakit1' => 23];
+        $return = array_merge($return, $b);
+        $c = ['penyakit2' => 23];
+        $return = array_merge($return, $c);
+
+        return $return;
+    })->name('api.test');
+    
+    // Dashboard Kependudukan
+    Route::namespace('Dashboard')->group(function () {
+
+        Route::get('/api/dashboard/kependudukan', ['as' => 'dashboard.kekendudukan.getdata', 'uses' => 'DashboardController@getDashboardKependudukan']);
+    });
+
+    Route::get('/api/list-peserta-penduduk', function () {
+        return \App\Models\Penduduk::selectRaw('nik as id, nama as text, nik, nama, alamat, rt, rw, tempat_lahir, tanggal_lahir')
+            ->whereRaw('lower(nama) LIKE \'%' . strtolower(request('q')) . '%\' or lower(nik) LIKE \'%' . strtolower(request('q')) . '%\'')->paginate(10);
+    });
+
+    Route::get('/api/list-peserta-kk', function () {
+        return \App\Models\Penduduk::selectRaw('no_kk as id, nama as text, nik, nama, alamat, rt, rw, tempat_lahir, tanggal_lahir')
+            ->whereRaw('lower(nama) LIKE \'%' . strtolower(request('q')) . '%\' or lower(no_kk) LIKE \'%' . strtolower(request('q')) . '%\'')
+            ->where('kk_level', 1)->paginate(10);
+    });
+    
+});

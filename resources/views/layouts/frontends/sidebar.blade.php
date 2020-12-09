9847e683e95562b8eb6e1cd79d776f8533a66f19
dirgareborn <kikifatiroh@gmail.com>
2020-12-09 16:26:21 +0800

    Template Halaman Pengunjung

---
+++ resources/views/layouts/frontends/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="col-md-4 col-sm-6" style="position: relative;">
+    <div class="box box-widget">
+        @include('widgets.komplain')
+    </div>
+    <div class="box box-widget">
+        @include('widgets.event')
+    </div>
+
+    <div class="box box-widget">
+        @include('widgets.visitor')
+    </div>
+</div>

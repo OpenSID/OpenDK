4edd6392f39cec12215814e364ed2d77cf6a29e2
dirgareborn <kikifatiroh@gmail.com>
2020-12-10 17:53:16 +0800

    update asset jquery social media share

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,6 +101,7 @@
     <!-- REQUIRED JS SCRIPTS -->
     <!-- jQuery 3 -->
     <script src="{{ asset('/bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('/bower_components/jquery/dist/jquery.socialShare.js') }}"></script>
     <!-- Bootstrap 3.3.7 -->
     <script src="{{ asset('/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
     <!-- AdminLTE App -->

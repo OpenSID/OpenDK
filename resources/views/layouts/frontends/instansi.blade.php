c711c96fec161f625ee91f6f0c567d2f0365cba4
dirgareborn <kikifatiroh@gmail.com>
2020-12-14 22:03:33 +0800

    up

--- resources/views/layouts/frontends/instansi.blade.php
+++ resources/views/layouts/frontends/instansi.blade.php
@@ -1,12 +1,12 @@
 <section class="regular slider" data-arrows="true">
-  <div class=""><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image1.png"></div>
-        <div class=""><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"></div>
-        <div class=""><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"></div>
-        <div class=""><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png"></div>
-        <div class=""><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image5.png"></div>
-        <div class=""><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png"></div>
-        <div class=""><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image7.png"></div>
-        <div class=""><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"></div>
+        <div class=""><a href="https://jenepontokab.go.id" target="_blank"><img src="{{ asset('/storage/instansi/pemkab.png') }}"></a></div> 
+        <div class=""><img src="{{ asset('/storage/instansi/pemkab.png') }}"></div>
+        <div class=""><img src="{{ asset('/storage/instansi/pemkab.png') }}"></div>
+        <div class=""><img src="{{ asset('/storage/instansi/pemkab.png') }}"></div>
+        <div class=""><img src="{{ asset('/storage/instansi/pemkab.png') }}"></div>
+        <div class=""><img src="{{ asset('/storage/instansi/pemkab.png') }}"></div>
+        <div class=""><img src="{{ asset('/storage/instansi/pemkab.png') }}"></div>
+        <div class=""><img src="{{ asset('/storage/instansi/pemkab.png') }}"></div>
       </section>
 <div class="clearfix"></div>
       

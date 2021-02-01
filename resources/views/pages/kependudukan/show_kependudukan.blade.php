6c1a7700e996d131a6fc7ba5af9e7a98cdca9eba
dirgareborn <kikifatiroh@gmail.com>
2021-02-02 05:44:39 +0800

    Update show_kependudukan.blade.php

--- resources/views/pages/kependudukan/show_kependudukan.blade.php
+++ resources/views/pages/kependudukan/show_kependudukan.blade.php
@@ -110,7 +110,7 @@
     </div>
     <!-- /.row -->
     <div class="row">
-        <div class="col-md-7">
+        <div class="col-md-12">
             <div class="box box-success">
                 <div class="box-header with-border">
                     <h3 class="box-title">Grafik Penduduk Tiap Tahun</h3>

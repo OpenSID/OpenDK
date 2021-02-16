ad4a030cf9c3331e1815ac6d8014cb8d45af0780
dirgareborn <kikifatiroh@gmail.com>
2021-02-16 09:34:50 +0800

    Data Jabatan

---
+++ database/migrations/2021_02_15_005139_create_jabatan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJabatanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('das_jabatan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_jabatan');
+            $table->integer('parent_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('das_jabatan');
+    }
+}
---
+++ resources/views/data/jabatan/create.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.dashboard_template')
+
+@section('content')
+        <!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        {{ $page_title ?? "Page Title" }}
+        <small>{{ $page_description ?? '' }}</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{route('dashboard.profil')}}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="{{route('data.jabatan.index')}}">Jabatan</a></li>
+        <li class="active">{{$page_title}}</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-primary">
+                {{-- <div class="box-header with-border">
+                     <h3 class="box-title">Aksi</h3>
+                 </div>--}}
+                <!-- /.box-header -->
+
+                <!-- form start -->
+                {!! Form::open( [ 'route' => 'data.jabatan.store', 'method' => 'post','id' => 'form-event', 'class' => 'form-horizontal form-label-left' ] ) !!}
+
+                <div class="box-body">
+
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    @include( 'flash::message' )
+                    @include('data.jabatan.form_create')
+
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <div class="pull-right">
+                        <div class="control-group">
+                            <a href="{{ route('data.jabatan.index') }}">
+                                <button type="button" class="btn btn-default btn-sm"><i class="fa fa-refresh"></i> Batal</button>
+                            </a>
+                            <button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-save"></i> Simpan</button>
+                        </div>
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</section>
+<!-- /.content -->
+@endsection
+
+@include(('partials.asset_wysihtml5'))
+@include(('partials.asset_datetimepicker'))
+@push('scripts')
+<script>
+    $(function () {
+        // Replace the <textarea id="editor1"> with a CKEditor
+        //bootstrap WYSIHTML5 - text editor
+        $('.textarea').wysihtml5()
+
+        //Datetimepicker
+        $('.datetime').each(function () {
+            var $this = $(this);
+            $this.datetimepicker({
+                format: 'YYYY-MM-D HH:mm'
+            });
+        });
+    })
+</script>
+@endpush

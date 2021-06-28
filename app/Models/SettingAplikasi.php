d8f2a8abaaec7adbd33016694b7d9fd72579b50c
ahmadrosid <alahmadrosid@gmail.com>
2021-06-28 22:51:07 +0700

    feat: add setting applikasi table

---
+++ app/Models/SettingAplikasi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SettingAplikasi extends Model
+{
+    protected $table = 'das_setting';
+
+    protected $fillable = ['key','value','type','description','option'];
+}
---
+++ database/migrations/2021_06_28_142557_create_setting_aplikasis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingAplikasisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('das_setting', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('key', 50)->unique();
+            $table->text('value');
+            $table->string('type', 30);
+            $table->string('description', 200);
+            $table->json('option');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seting_aplikasis');
+    }
+}

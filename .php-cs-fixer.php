e31580371ee97fcc828a0bbee111ed1e2f97bfa2
afa28 <andifahruddinakas@gmail.com>
2021-09-21 17:49:58 +0800

    php-fixer with header license

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,22 +2,38 @@
 
 declare(strict_types=1);
 
-$header = <<<'EOF'
-@package	OpenDK
-@author	opendesa
-@license	https://opensource.org/licenses/MIT	MIT License
-@link	https://github.com/OpenSID/OpenDK
-@since	Version v21.08.01
-@filesource
+$tahun = date('Y');
+
+$header = <<<EOF
+File ini bagian dari:
+
+PBB Desa
+
+Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+
+Hak Cipta 2016 - $tahun Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+
+Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+asal tunduk pada syarat berikut:
+
+Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+
+PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+
+@package	    OpenDK
+@author	    Tim Pengembang OpenDesa
+@copyright	Hak Cipta 2016 - $tahun Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+@license    	http://www.gnu.org/licenses/gpl.html    GPL V3
+@link	    https://github.com/OpenSID/opendk
 EOF;
 
 $finder = PhpCsFixer\Finder::create()
-    // ->in([
-    //     __DIR__ . '/app',
-    //     __DIR__ . '/helpers',
-    //     __DIR__ . '/routes',
-    //     // __DIR__ . '/database', // Timeout
-    // ])
     ->in(__DIR__)
     ->exclude([
         'bootstrap',
@@ -39,7 +55,7 @@ return $config->setRules([
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
-        // 'header_comment' => ['header' => $header],
+        'header_comment' => ['header' => $header],
     ])
     ->setUsingCache(false)
     ->setLineEnding(PHP_EOL)

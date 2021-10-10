84eacb3fc785ceacd28c23e7784c1d9d1f1bc4b6
Muhammad Irwan Andriawan <irwan@andriawan.com>
2021-10-10 21:06:52 +0700

    initial pesan and pesan detail table migration

---
+++ database/migrations/2021_10_10_202406_create_table_detail_pesan.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableDetailPesan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('das_pesan_detail', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('pesan_id');
+            $table->text('text');
+            $table->unsignedInteger('desa_id');
+            $table->foreign('pesan_id')
+                ->references('id')
+                ->on('das_pesan')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('desa_id')
+                ->references('id')
+                ->on('das_data_desa')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('das_pesan_detail');
+        Schema::enableForeignKeyConstraints();
+    }
+}

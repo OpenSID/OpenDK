bcb1749ae39a4510069adac0134ffeb21c16e7ad
andifahruddinakas <andifahruddinakas@users.noreply.github.com>
2021-11-17 15:20:12 +0000

    Fix styling

--- database/seeds/DemoDatabaseSeeder.php
+++ database/seeds/DemoDatabaseSeeder.php
@@ -29,23 +29,23 @@
  * @link	    https://github.com/OpenSID/opendk
  */
 
-use Illuminate\Database\Seeder;
-use Database\Seeds\Demo\DemoPesanSeeder;
 use Database\Seeds\Demo\DemoAKIAKBSeeder;
+use Database\Seeds\Demo\DemoAnggaranRealisasiSeeder;
 use Database\Seeds\Demo\DemoAPBDesaSeeder;
-use Database\Seeds\Demo\DemoPendudukSeeder;
-use Database\Seeds\Demo\DemoImunisasiSeeder;
-use Database\Seeds\Demo\DemoPesanDetailSeeder;
-use Database\Seeds\Demo\DemoPutusSekolahSeeder;
-use Database\Seeds\Demo\DemoFasilitasPaudSeeder;
-use Database\Seeds\Demo\DemoDasProfilTableSeeder;
-use Database\Seeds\Demo\DemoToiletSanitasiSeeder;
-use Database\Seeds\Demo\DemoEpidemiPenyakitSeeder;
 use Database\Seeds\Demo\DemoDasDataDesaTableSeeder;
 use Database\Seeds\Demo\DemoDasDataUmumTableSeeder;
 use Database\Seeds\Demo\DemoDasKeluargaTableSeeder;
-use Database\Seeds\Demo\DemoAnggaranRealisasiSeeder;
+use Database\Seeds\Demo\DemoDasProfilTableSeeder;
+use Database\Seeds\Demo\DemoEpidemiPenyakitSeeder;
+use Database\Seeds\Demo\DemoFasilitasPaudSeeder;
+use Database\Seeds\Demo\DemoImunisasiSeeder;
+use Database\Seeds\Demo\DemoPendudukSeeder;
+use Database\Seeds\Demo\DemoPesanDetailSeeder;
+use Database\Seeds\Demo\DemoPesanSeeder;
+use Database\Seeds\Demo\DemoPutusSekolahSeeder;
 use Database\Seeds\Demo\DemoTingkatPendidikanSeeder;
+use Database\Seeds\Demo\DemoToiletSanitasiSeeder;
+use Illuminate\Database\Seeder;
 
 class DemoDatabaseSeeder extends Seeder
 {

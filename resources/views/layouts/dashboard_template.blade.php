ee684dbdef69e54f336ad74700114aa9ff34e5c0
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-12-31 16:18:16 +0800

    Update dashboard_template.blade.php

--- resources/views/layouts/dashboard_template.blade.php
+++ resources/views/layouts/dashboard_template.blade.php
@@ -60,7 +60,7 @@ desired effect
 |               | sidebar-mini                            |
 |---------------------------------------------------------|
 -->
-<body class="hold-transition skin-blue sidebar-mini">
+<body class="hold-transition skin-blue sidebar-mini fixed">
 <div class="wrapper">
 
     <!-- Main Header -->

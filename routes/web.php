0f24aeacfec88ac5aedcef13149243a920f0bb87
Afila <apidongblog@gmail.com>
2022-03-15 16:13:17 +0700

    Update routes/web.php
    Co-authored-by: Agung Sugiarto <sugiartoagung92@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -599,23 +599,3 @@ Route::group(['middleware' => 'installed'], function () {
             ->paginate(10);
     });
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');

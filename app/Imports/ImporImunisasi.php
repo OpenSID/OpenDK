6fbaf381c6353a9d3ee07b2ce91ec117e212a1f8
apidong <apidong@users.noreply.github.com>
2022-03-28 15:09:53 +0000

    Fix styling

--- app/Imports/ImporImunisasi.php
+++ app/Imports/ImporImunisasi.php
@@ -31,20 +31,19 @@
 
 namespace App\Imports;
 
-use Exception;
 use App\Models\DataDesa;
-// use Maatwebsite\Excel\Concerns\ToModel;
 use App\Models\Imunisasi;
+// use Maatwebsite\Excel\Concerns\ToModel;
+use Exception;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Arr;
-use Illuminate\Validation\Rule;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\Importable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Maatwebsite\Excel\Concerns\ToCollection;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 class ImporImunisasi implements ToCollection, WithHeadingRow, WithChunkReading, ShouldQueue
 {

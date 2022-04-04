db21dd9d72e0ee4df33356d0a61b08891d1fe378
Afila <apidongblog@gmail.com>
2022-04-04 14:02:02 +0700

    - perbaiki import

--- app/Imports/SinkronPenduduk.php
+++ app/Imports/SinkronPenduduk.php
@@ -108,6 +108,8 @@ class SinkronPenduduk implements ToCollection, WithHeadingRow, WithChunkReading,
                 'desa_id'      => $insert['desa_id'],
                 'id_pend_desa' => $insert['id_pend_desa']
             ], $insert);
+
+            
         }
     }
 }

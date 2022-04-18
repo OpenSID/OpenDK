1d46bbb2dff8efe838e31d2ab0691ec738eda11d
Afila <apidongblog@gmail.com>
2022-04-18 07:43:05 +0700

    migrasi

---
+++ database/migrations/2022_04_17_225617_create_table_das_pembangunan.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableDasPembangunan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('das_pembangunan', function (Blueprint $table) {
+            $table->integer('id', false);
+            $table->char('id_desa', 13);
+            $table->string('lokasi', 255)->nullable();
+            $table->string('judul', 255)->nullable();
+            $table->string('slug', 255)->nullable();
+            $table->string('keterangan', 255)->nullable();
+            $table->string('volume', 255)->nullable();
+            $table->year('tahun_anggaran')->nullable();
+            $table->string('pelaksana_kegiatan', 255)->nullable();
+            $table->tinyInteger('status')->nullable();
+            $table->string('foto', 255)->nullable();
+            $table->float('anggaran', 65, 2)->nullable();
+            $table->float('perubahan_anggaran', 65, 2)->nullable();
+            $table->float('sumber_biaya_pemerintah', 65, 2)->nullable();
+            $table->float('sumber_biaya_provinsi', 65, 2)->nullable();
+            $table->float('sumber_biaya_kab_kota', 65, 2)->nullable();
+            $table->float('sumber_biaya_swadaya', 65, 2)->nullable();
+            $table->float('sumber_biaya_jumlah', 65, 2)->nullable();
+            $table->string('manfaat', 100)->nullable();
+            $table->integer('waktu')->nullable();
+            $table->timestamps();
+            $table->unique(['id', 'id_desa']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('das_pembangunan');
+    }
+}
---
+++ database/migrations/2022_04_18_070823_create_table_das_pembangunan_dokumentasi.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableDasPembangunanDokumentasi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('das_pembangunan_dokumentasi', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('id_pembangunan');
+            $table->char('id_desa', 13);
+            $table->string('gambar', 255)->nullable();
+            $table->string('persentase', 255)->nullable();
+            $table->string('keterangan', 255)->nullable();
+            $table->timestamps();
+            $table->unique(['id', 'id_desa', 'id_pembangunan']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_das_pembangunan_dokumentasi');
+    }
+}

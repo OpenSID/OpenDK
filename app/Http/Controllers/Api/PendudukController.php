1dcaff6855118d05dc9592e93daff5b7f3cdf3d3
Afila <apidongblog@gmail.com>
2022-05-12 19:36:17 +0700

    #72 Amankan route api penduduk dengan middleware.

--- app/Http/Controllers/Api/PendudukController.php
+++ app/Http/Controllers/Api/PendudukController.php
@@ -49,7 +49,7 @@ class PendudukController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware(['auth:api', 'role:admin-desa']);
     }
 
     /**

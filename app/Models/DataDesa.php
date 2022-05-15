2cc3424248dd67fc96c5b789aa65d2f6c3b394f1
Andi Fahruddin Akas <57283157+andifahruddinakas@users.noreply.github.com>
2022-05-15 13:46:11 +0800

    Sesuaikan sebutan desa yang tidak muncul.

--- app/Models/DataDesa.php
+++ app/Models/DataDesa.php
@@ -54,7 +54,7 @@ class DataDesa extends Model
     {
         $desa = [
             'desa_id' => $this->desa_id,
-            'nama' => 'Desa ' . $this->nama,
+            'nama'    => ucwords($this->sebutan_desa . ' ' . $this->nama),
             'website' => $this->website . 'index.php/feed'
         ];
         return $desa;

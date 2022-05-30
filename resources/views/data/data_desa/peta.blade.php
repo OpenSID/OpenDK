0c96bea91bbf44cabf78ea93c6acd3c9449b5b0e
Afila <apidongblog@gmail.com>
2022-05-30 14:41:55 +0700

    perbaiki peta tidak tampil (#419)

--- resources/views/data/data_desa/peta.blade.php
+++ resources/views/data/data_desa/peta.blade.php
@@ -19,7 +19,7 @@
       <div class="row">
         <div class="col-md-12">
             <div id="tampil-map" style="height: calc(100VH - 110px);">
-              <div class="text-center" style="margin-top: 35vh"><h1>Peta Tidak Tersedia</h1></div>
+              <div class="text-center" style="margin-top: 35vh"><h1>Memuat Peta</h1></div>
             </div>
             <input type="hidden" id="path">
         </div>
@@ -42,6 +42,9 @@
       });
 
       var path_desa = {{ empty($desa->path)? 'new Array()': $desa->path }};
+      if (path_desa.length == 0) {
+        $('#tampil-map div.text-center').html(`<h1>Peta Tidak Tersedia</h1>`)
+      }
       // Geolocation IP Route/GPS
       geoLocation(peta_wilayah);
       showPolygon(path_desa, peta_wilayah)
@@ -54,7 +57,6 @@
     };
 
     $(function () {
-
       $('#list_desa').change(function () {
           $("#desa_id").val($('#list_desa').val());
           $("#nama").val($('#list_desa option:selected').text());
@@ -76,11 +78,11 @@
       }
       
       $.when(path_kec()).done(function(res_kec){
-        if (res_kec) {
+        if (res_kec && res_kec.data != null) {
           var mark_kec = set_marker(res_kec.data, 'Peta Wilayah Kecamatan', 'Wilayah Kecamatan ' + res_kec.data.profil.nama_kecamatan, {'line' : '#de2d26', 'fill' : '#fff'});
           overlayLayers['Peta Wilayah Kecamatan'] =  wilayah_property(mark_kec, false);
-          tampil_peta();
         }
+        tampil_peta();
       });
     });
 </script>

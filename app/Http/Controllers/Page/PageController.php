6ac39007fc075d0d18f025a23e9c6aff283450ed
Yogi Perdana <yogiperdana3@gmail.com>
2022-07-08 08:48:24 +0700

    Tambah tombol filter berdasarkan tanggal

--- app/Http/Controllers/Page/PageController.php
+++ app/Http/Controllers/Page/PageController.php
@@ -122,6 +122,13 @@ class PageController extends Controller
         }
 
         $feeds = $feeds->sortByDesc('date')->take(config('setting.jumlah_artikel_desa') ?? 30)->paginate(config('setting.artikel_desa_perhalaman') ?? 10);
+        
+        // Tanggal
+        $tanggal = $request->tanggal;
+        if ($tanggal != '0') {
+            $feeds = $feeds->sortBy('date')->take(config('setting.jumlah_artikel_desa') ?? 30)->paginate(config('setting.artikel_desa_perhalaman') ?? 10);
+        }
+
         $feeds->all();
 
         $html =  view('pages.berita.feeds', [

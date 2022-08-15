1aedd8916567710b5f47b08cc542223e0ad34f72
Yogi Perdana <yogiperdana3@gmail.com>
2022-08-15 20:45:51 +0700

    Disable form penduduk sebelum desa dipilih

--- resources/views/data/data_suplemen/form_detail.blade.php
+++ resources/views/data/data_suplemen/form_detail.blade.php
@@ -53,7 +53,7 @@
         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="penduduk_id">{{ $suplemen->sasaran == 2 ? 'Nama Kepala Keluarga' : 'Nama Penduduk' }}</label>
         
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <select name="penduduk_id" id="penduduk" class="form-control">
+            <select name="penduduk_id" id="penduduk" class="form-control" disabled>
                 <option class="form-control" value="">Pilih Penduduk</option>
                 @foreach ($data as $penduduk)
                     @if ($anggota == null)

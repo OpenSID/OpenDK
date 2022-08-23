c12cfb8197e181becad7742e5ae3f05f5f4d52cd
Yogi Perdana <yogiperdana3@gmail.com>
2022-08-23 19:33:33 +0700

    Perbaiki padding dan margin widget

--- resources/views/widgets/sinergi_program.blade.php
+++ resources/views/widgets/sinergi_program.blade.php
@@ -1,9 +1,9 @@
 <div class="box-header with-border text-center  bg-blue">
     <h2 class="box-title text-bold">Sinergi Program</h2>
 </div>
-<div class="row">
+<div class="row" style="margin: 0">
     @foreach($sinergi as $key => $data)
-        <div class="col-md-6">
+        <div class="col-md-6" style="padding: 4px;">
             <a href="{{ $data->url }}" rel="noopener noreferrer" target="_blank">
                 <img src="{{ $data->gambar }}" alt="Sinergi Program Image" style="max-width:100%; height:auto;">
             </a>

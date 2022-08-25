6a4531b494446d5e52b13c2881cd13e020e746c9
Yogi Perdana <yogiperdana3@gmail.com>
2022-08-25 10:39:15 +0700

    Perbaiki urutan widget

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -75,7 +75,7 @@ class Controller extends BaseController
         $this->browser_title = SettingAplikasi::where('key', 'judul_aplikasi')->first()->value ?? ucwords($this->sebutan_wilayah . ' ' . $this->profil->nama_kecamatan);
 
         $events                      = Event::getOpenEvents();
-        $sinergi                      = SinergiProgram::where('status', 1)->get();
+        $sinergi                     = SinergiProgram::where('status', 1)->orderBy('urutan', 'asc')->get();
         $medsos                      = MediaSosial::where('status', 1)->get();
         $navdesa                     = DataDesa::all();
         $navpotensi                  = TipePotensi::orderby('nama_kategori', 'ASC')->get();

b7f396b9256c4fa2db1f25ced6918fb8ac57ceb9
Yogi Perdana <yogiperdana3@gmail.com>
2022-11-10 09:12:06 +0700

    Buat model

--- app/Models/Pengurus.php
+++ app/Models/Pengurus.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pengurus extends Model
 {
-    //
+    protected $table = 'das_pengurus';
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    protected $with = [
+        'jabatan'
+    ];
+
+    public function jabatan()
+    {
+        return $this->hasOne(DataDesa::class, 'jabatan_id', 'id');
+    }
 }

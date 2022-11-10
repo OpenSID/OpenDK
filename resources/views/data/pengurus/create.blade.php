9db1532e4097a31701323b8a47d6d30bae0b805a
Yogi Perdana <yogiperdana3@gmail.com>
2022-11-10 09:25:35 +0700

    Sesuaikan menu

---
+++ resources/views/data/pengurus/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.dashboard_template')
+
+@section('content')
+<section class="content-header">
+    <h1>
+        {{ $page_title ?? "Page Title" }}
+        <small>{{ $page_description ?? '' }}</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="{{ route('data.jabatan.index') }}">Daftar Jabatan</a></li>
+        <li class="active">{{ $page_description }}</li>
+    </ol>
+</section>
+
+<section class="content container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-primary">
+
+                {!! Form::open( [ 'route' => 'data.jabatan.store', 'method' => 'post','id' => 'form-jabatan', 'class' => 'form-horizontal form-label-left' ] ) !!}
+
+                <div class="box-body">
+
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Ups!</strong> Ada beberapa masalah dengan masukan Anda.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    @include( 'flash::message' )
+                    @include('data.jabatan.form')
+
+                </div>
+                <div class="box-footer">
+                    <div class="form-group">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                            <a href="{{ route('data.jabatan.index') }}">
+                                <button type="button" class="btn btn-default btn-sm"><i class="fa fa-refresh"></i>&nbsp; Batal</button>
+                            </a>
+                            <button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-save"></i>&nbsp; Simpan</button>
+                        </div>
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
---
+++ resources/views/data/pengurus/form.blade.php
@@ -0,0 +1,16 @@
+<div class="form-group">
+    <label class="control-label col-md-3 col-sm-3 col-xs-12">Nama Jabatan <span class="required">*</span></label>
+
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        {!! Form::text('nama', null, ['placeholder' => 'Nama Jabatan','class' => 'form-control', 'required'=>true]) !!}
+        {{ Form::hidden('jenis') }}
+    </div>
+</div>
+<div class="form-group">
+    <label class="control-label col-md-3 col-sm-3 col-xs-12">Tupoksi</label>
+
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        {!! Form::textarea('tupoksi', null, ['class' => 'textarea', 'placeholder' => 'Tupoksi', 'style' => 'width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;']) !!}
+    </div>
+</div>
+<div class="ln_solid"></div>

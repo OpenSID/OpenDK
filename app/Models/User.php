86463d526bd7eafe26110cd4261b0125cba6f0be
Yogi Perdana <yogiperdana3@gmail.com>
2022-11-23 14:49:01 +0700

    Buat preview foto di user

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,14 +31,15 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable as AuthenticableTrait;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
 use Intervention\Image\Facades\Image;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Facades\Storage;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 
 class User extends Authenticatable implements JWTSubject
 {
@@ -97,6 +98,11 @@ class User extends Authenticatable implements JWTSubject
     public static function datatables()
     {
         return static::select('name', 'address', 'status', 'id', 'email', 'created_at', 'phone');
+    }
+
+    public function getFotoAttribute()
+    {
+        return $this->attributes['image'] ? Storage::url('user/' . $this->attributes['image']) : null;
     }
 
     /**

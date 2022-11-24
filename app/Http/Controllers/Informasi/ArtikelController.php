4bacdd295aecc0aad993904147f17ee12581f484
Yogi Perdana <yogiperdana3@gmail.com>
2022-11-24 17:00:09 +0700

    Perbaiki tanggal terbit artikel

--- app/Http/Controllers/Informasi/ArtikelController.php
+++ app/Http/Controllers/Informasi/ArtikelController.php
@@ -66,6 +66,8 @@ class ArtikelController extends Controller
                     } else {
                         return '<span class="label label-success">Aktif</span>';
                     }
+                })->editColumn('created_at', function ($row) {
+                    return format_datetime($row->created_at);
                 })
                 ->rawColumns(['status'])
                 ->make(true);

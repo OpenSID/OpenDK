eeafbde6205785f7ea1b673b65258076e720b1df
Yogi Perdana <yogiperdana3@gmail.com>
2022-12-16 16:58:19 +0700

    Perbaiki breadcrumb

--- resources/views/surat/pengaturan.blade.php
+++ resources/views/surat/pengaturan.blade.php
@@ -7,7 +7,8 @@
             <small>{{ $page_description ?? '' }}</small>
         </h1>
         <ol class="breadcrumb">
-            <li class="active"><i class="fa fa-dashboard"></i> Pengaturan</li>
+            <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active">{{ $page_title }}</li>
         </ol>
     </section>
     <section class="content">

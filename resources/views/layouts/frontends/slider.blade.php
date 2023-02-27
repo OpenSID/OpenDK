1369712796ffa92b3bd0ec2fb702fb4f769342a8
Yogi Perdana <yogiperdana3@gmail.com>
2023-02-27 23:16:14 +0700

    Perbaiki slider tidak bisa lebih dari 4

--- resources/views/layouts/frontends/slider.blade.php
+++ resources/views/layouts/frontends/slider.blade.php
@@ -1,86 +1,51 @@
 @if (Route::currentRouteName() === 'beranda')
 <!-- Slider -->
-<div id="slider">
-    <div class="slides">
-        @php $slides  = \App\Models\Slide::orderBy('created_at','DESC')->take(4)->get(); @endphp
-        @forelse ($slides as $slide)
-        
-        <div class="slider-class">
-            <div class="legend"></div>
-            <div class="content-slide">
-                <div class="content-txt">
-                    <h1>{{ $slide->judul }}</h1>
-                    <h2>{{ $slide->deskripsi }}</h2>
+<div id="swiper-slider" class="swiper">
+    <div class="swiper-wrapper">
+        @php $slides  = \App\Models\Slide::orderBy('created_at','DESC')->get(); @endphp
+        @foreach ($slides as $slide)
+        <div class="swiper-slide">
+            <div class="slider-class">
+                <div class="legend"></div>
+                <div class="content-slide">
+                    <div class="content-txt">
+                        <h1>{{ $slide->judul }}</h1>
+                        <h2>{{ $slide->deskripsi }}</h2>
+                    </div>
+                </div>
+                <div class="image">
+                    <img src="{{ asset($slide->gambar) }}">
                 </div>
             </div>
-            <div class="image">
-                <img src="{{ asset($slide->gambar) }}">
-            </div>
         </div>
-        @empty          
-        <div class="slider-class">
-            <div class="legend"></div>
-            <div class="content-slide">
-                <div class="content-txt">
-                    <h1>Pantai Garassikang</h1>
-                    <h2>Lokasi: Bulu Jaya, Kecamatan Bangkala Barat, Kabupaten Jeneponto, Sulawesi Selatan</h2>
-                </div>
-            </div>
-            <div class="image">
-                <img src="{{ asset('/slide/slide-1.png') }}">
-            </div>
-        </div>
-        <div class="slider-class">
-            <div class="legend"></div>
-            <div class="content-slide">
-                <div class="content-txt">
-                    <h1>Batu Siping</h1>
-                    <h2>Lokasi: Karampuang, Desa Garassikang, Kecamatan Bangkala Barat, Kabupaten Jeneponto, Sulawesi Selatan.</h2>
-                </div>
-            </div>
-            <div class="image">
-                <img src="{{ asset('/slide/slide-2.png') }}">
-            </div>
-        </div>
-        <div class="slider-class">
-            <div class="legend"></div>
-            <div class="content-slide">
-                <div class="content-txt">
-                    <h1>Bukit Sinalu Bulu Jaya</h1>
-                    <h2>Lokasi: Bulu Jaya, Kecamatan Bangkala Barat, Kabupaten Jeneponto, Sulawesi Selatan</h2>
-                </div>
-            </div>
-            <div class="image">
-                <img src="{{ asset('/slide/slide-4.png') }}">
-            </div>
-        </div>
-        <div class="slider-class">
-            <div class="legend"></div>
-            <div class="content-slide">
-                <div class="content-txt">
-                    <h1>Pantai Tamarunang</h1>
-                    <h2>Lokasi: Tamarunang, Pabiringa, Kecamatan Binamu, Kabupaten Jeneponto, Sulawesi Selatan</h2>
-                </div>
-            </div>
-            <div class="image">
-                <img src="{{ asset('/slide/slide-3.png') }}">
-            </div>
-        </div>
-        @endforelse
-        
-
+        @endforeach
     </div>
-    <div class="switch">
-        <ul>
-            <li>
-                <div class="on"></div>
-            </li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </div>
-</div>
-<div class="dk-line">
-</div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+  </div>
+  <div class="dk-line"></div>
 @endif
+
+@push('scripts')
+  <script>
+    $(document).ready(function() {
+      var swiper = new Swiper("#swiper-slider", {
+        autoplay: {
+            delay: 4000,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+      });
+    });
+  </script>    
+  @endpush

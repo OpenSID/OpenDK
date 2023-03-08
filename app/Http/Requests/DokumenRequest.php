bcffc364dd0e63d82a0b39b683f6bcf723f73eef
Yogi Perdana <yogiperdana3@gmail.com>
2023-03-08 13:14:49 +0700

    Tambah validasi valid file

--- app/Http/Requests/DokumenRequest.php
+++ app/Http/Requests/DokumenRequest.php
@@ -54,7 +54,7 @@ class DokumenRequest extends FormRequest
     {
         return [
             'nama_dokumen' => 'required|string|max:255',
-            'file_dokumen' => 'file|mimes:jpeg,png,jpg,gif,svg,xlsx,xls,doc,docx,pdf,ppt,pptx|max:2048',
+            'file_dokumen' => 'file|mimes:jpeg,png,jpg,gif,svg,xlsx,xls,doc,docx,pdf,ppt,pptx|max:2048|valid_file',
         ];
     }
 }

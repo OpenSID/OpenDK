aeb9f3d09cda7307987dcd43fc9f37fb3aa44d6a
Yogi Perdana <yogiperdana3@gmail.com>
2023-05-30 21:32:56 +0700

    Sesuaikan config

--- config/lfm.php
+++ config/lfm.php
@@ -58,6 +58,7 @@ return [
                 'image/pjpeg',
                 'image/png',
                 'image/gif',
+                'image/svg+xml',
                 'application/pdf',
                 'text/plain',
             ],
@@ -74,6 +75,7 @@ return [
                 'image/pjpeg',
                 'image/png',
                 'image/gif',
+                'image/svg+xml',
             ],
         ],
     ],
@@ -160,6 +162,21 @@ return [
         'png'  => 'PNG Image',
         'ppt'  => 'Microsoft PowerPoint',
         'pptx' => 'Microsoft PowerPoint',
+    ],
+
+    'file_icon_array' => [
+        'pdf'  => 'fa-file-pdf-o',
+        'doc'  => 'fa-file-word-o',
+        'docx' => 'fa-file-word-o',
+        'xls'  => 'fa-file-excel-o',
+        'xlsx' => 'fa-file-excel-o',
+        'zip'  => 'fa-file-archive-o',
+        'gif'  => 'fa-file-image-o',
+        'jpg'  => 'fa-file-image-o',
+        'jpeg' => 'fa-file-image-o',
+        'png'  => 'fa-file-image-o',
+        'ppt'  => 'fa-file-powerpoint-o',
+        'pptx' => 'fa-file-powerpoint-o',
     ],
 
     /*

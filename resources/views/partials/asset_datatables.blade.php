ad7f876a5eed4935eed702b9b1a16118ba02398d
Agung Sugiarto <sugiartoagung92@gmail.com>
2023-07-18 10:38:22 +0700

    [bug] default bahasa indonesia untuk datatable

--- resources/views/partials/asset_datatables.blade.php
+++ resources/views/partials/asset_datatables.blade.php
@@ -7,4 +7,9 @@
 <!-- DataTables -->
 <script src="{{ asset ("/bower_components/datatables.net/js/jquery.dataTables.min.js") }}"></script>
 <script src="{{ asset ("/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js") }}"></script>
+<script>
+    $.extend($.fn.dataTable.defaults, {
+        language: { url: "{{ asset('/bower_components/datatables.net/i18n/id.json') }}" }
+    });
+</script>
 @endpush

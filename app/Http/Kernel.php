c0d42702fb660577b3e2fe55d5cbfe658f04dbfa
Yogi Perdana <yogiperdana3@gmail.com>
2023-08-17 09:14:05 +0700

    pembatasan input element HTML (#787)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -120,6 +120,7 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'xss_sanitization' => \App\Http\Middleware\XssSanitization::class,
     ];
 
     /**

14911eda2d3a6d65eea2a5b496c7a0b06c59f100
Toto <totoprayogo1916@gmail.com>
2023-09-02 07:19:24 +0700

    kembalikan after()

--- database/migrations/2022_04_20_105732_tambahkan_kolom_programbantuan.php
+++ database/migrations/2022_04_20_105732_tambahkan_kolom_programbantuan.php
@@ -43,8 +43,8 @@ class TambahkanKolomProgrambantuan extends Migration
     public function up()
     {
         Schema::table('das_program', function (Blueprint $table) {
-            $table->char('desa_id', 13)->nullable();
-            $table->tinyInteger('status', false, 1)->nullable();
+            $table->char('desa_id', 13)->after('sasaran')->nullable();
+            $table->tinyInteger('status', false, 1)->after('desa_id')->nullable();
         });
     }
 

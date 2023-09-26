e0d2078e9dff7ff0082bd7485876a492c6ca653f
Yogi Perdana <yogiperdana3@gmail.com>
2023-09-26 11:23:58 +0700

    Sesuaikan notifikasi profil belum lengkap

--- resources/views/partials/flash_message.blade.php
+++ resources/views/partials/flash_message.blade.php
@@ -1,6 +1,6 @@
 @php $user = auth()->user(); @endphp
 
-@if (isset($user) && !$profil->kecamatan_id)
+@if ((isset($user) && !$profil->kecamatan_id) && !Request::is('changedefault'))
     <div class="callout callout-warning">
         <h4><i class="icon fa fa-warning"></i> Peringatan!</h4>
         <p>Data profil wilayah belum lengkap. Silahkan dilengkapi terlebih dahulu!. <a href="{{ route('data.profil.index') }}">Lengkapi</a></p>

6ae2ec797f6132634d95de4de88a9b45d4b9742e
Yogi Perdana <yogiperdana3@gmail.com>
2023-10-10 11:32:30 +0700

    Sesuaikan lebar seperti di opensid

--- resources/views/layouts/fragments/control_sidebar.blade.php
+++ resources/views/layouts/fragments/control_sidebar.blade.php
@@ -26,6 +26,3 @@
     </div>
 </aside>
 <!-- /.control-sidebar -->
-<!-- Add the sidebar's background. This div must be placed
-immediately after the control sidebar -->
-<div class="control-sidebar-bg"></div>

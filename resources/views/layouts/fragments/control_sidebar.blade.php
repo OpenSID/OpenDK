46957b843bd14c8dc8452b9ad2d82651b1bdf55b
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2023-10-11 11:22:04 +0800

    Update control_sidebar.blade.php

--- resources/views/layouts/fragments/control_sidebar.blade.php
+++ resources/views/layouts/fragments/control_sidebar.blade.php
@@ -1,26 +1,21 @@
 <!-- Control Sidebar -->
 <aside class="control-sidebar control-sidebar-light">
-    <!-- Create the tabs -->
-    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-        <li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-question-circle fa-lg"></i> Bantuan</a></li>
-    </ul>
     <!-- Tab panes -->
     <div class="tab-content">
-        <!-- Home tab content -->
         <div class="tab-pane active" id="control-sidebar-home-tab">
-            <p>Jika Anda butuh bantuan untuk menggunakan Aplikasi Dashboard Kecamatan, silahkan Anda Unduh Panduan Pengguna di bawah ini.</p>
-            <br>
-            <a href="{{ asset('storage/template_upload/Panduan_Pengguna_Kecamatan_Dashboard.pdf') }}" target="_blank" class="btn btn-primary btn-lg col-md-12"><i class="fa fa-download"></i> Unduh Panduan</a>
-            <!-- /.control-sidebar-menu -->
-            <br>
-            <br>
-            <br>
-            <h3>Catatan Rilis</h3>
+            <h3 class="text-center"><strong>Catatan Rilis</strong></h3>
             @php
                 $files = base_path('catatan_rilis.md');
                 $content = parsedown($files);
             @endphp
             {!! $content !!}
+
+            <hr style="border: 1px solid #ddd;">
+            <p>Jika Anda butuh bantuan untuk menggunakan Aplikasi Dashboard Kecamatan, silahkan Anda Unduh Panduan Pengguna di bawah ini.</p>
+            <br>
+            <a href="{{ asset('storage/template_upload/Panduan_Pengguna_Kecamatan_Dashboard.pdf') }}" target="_blank" class="btn btn-primary btn-lg col-md-12"><i class="fa fa-download"></i> Unduh Panduan</a>
+            <br>
+            <br>
         </div>
         <!-- /.tab-pane -->
     </div>

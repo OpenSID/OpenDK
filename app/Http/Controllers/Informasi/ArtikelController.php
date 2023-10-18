8604bf2dc50bf145add38ade991f993f73b310a0
yogiperdana <yogiperdana@users.noreply.github.com>
2023-10-18 07:56:53 +0000

    Fix styling by PHP-CS-FIXER

--- app/Http/Controllers/Informasi/ArtikelController.php
+++ app/Http/Controllers/Informasi/ArtikelController.php
@@ -31,12 +31,12 @@
 
 namespace App\Http\Controllers\Informasi;
 
-use App\Models\Artikel;
-use Illuminate\Http\Request;
-use Yajra\DataTables\DataTables;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ArtikelRequest;
+use App\Models\Artikel;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Yajra\DataTables\DataTables;
 
 class ArtikelController extends Controller
 {

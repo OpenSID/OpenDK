02af1b74038d0d1b1f6e52161018bd02976847bc
totoprayogo1916 <totoprayogo1916@users.noreply.github.com>
2023-10-27 22:58:30 +0000

    Blade Prettier Code

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,12 +1,36 @@
 <!doctype html>
 <title>Sedang dalam Perbaikan</title>
 <style>
-  body { text-align: center; padding: 150px; }
-  h1 { font-size: 50px; }
-  body { font: 20px Helvetica, sans-serif; color: #333; }
-  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-  a { color: #dc8100; text-decoration: none; }
-  a:hover { color: #333; text-decoration: none; }
+    body {
+        text-align: center;
+        padding: 150px;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
 </style>
 
 <article>

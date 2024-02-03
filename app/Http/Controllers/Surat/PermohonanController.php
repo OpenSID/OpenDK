0506de63fdc648e3b065c79daeb7d0e60906799d
pandigresik <pandigresik@users.noreply.github.com>
2024-02-03 01:22:54 +0000

    Fix styling by PHP-CS-FIXER

--- app/Http/Controllers/Surat/PermohonanController.php
+++ app/Http/Controllers/Surat/PermohonanController.php
@@ -7,7 +7,7 @@
  *
  * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
  *
- * Hak Cipta 2017 - 2023 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * Hak Cipta 2017 - 2024 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
  *
  * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
  * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
@@ -24,7 +24,7 @@
  *
  * @package    OpenDK
  * @author     Tim Pengembang OpenDesa
- * @copyright  Hak Cipta 2017 - 2023 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @copyright  Hak Cipta 2017 - 2024 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
  * @license    http://www.gnu.org/licenses/gpl.html    GPL V3
  * @link       https://github.com/OpenSID/opendk
  */
@@ -204,7 +204,7 @@ class PermohonanController extends Controller
                     ['name' => 'nik', 'contents' => $surat->pengurus->nik],
                     ['name' => 'passphrase', 'contents' => $request['passphrase']],
                     ['name' => 'tampilan', 'contents' => 'visible'],
-                    ['name' => 'linkQR', 'contents' => route('surat.arsip.qrcode', $surat->id)],                    
+                    ['name' => 'linkQR', 'contents' => route('surat.arsip.qrcode', $surat->id)],
                     ['name' => 'width', 'contents' => $width],
                     ['name' => 'height', 'contents' => $height],
                     ['name' => 'tag_koordinat', 'contents' => $tag],

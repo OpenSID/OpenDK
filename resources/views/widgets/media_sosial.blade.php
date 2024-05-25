84777f823932323aac40fc073675c05600d4a38d
Nyoman Jyotisa <73281575+nyomanjyotisa@users.noreply.github.com>
2024-05-25 11:28:07 +0800

    fix: justify content center list sosial media

--- resources/views/widgets/media_sosial.blade.php
+++ resources/views/widgets/media_sosial.blade.php
@@ -1,7 +1,7 @@
 <div class="box-header with-border text-center  bg-blue">
     <h2 class="box-title text-bold">Media Sosial</h2>
 </div>
-<ul style="list-style-type: none; display:flex; padding: 0;">
+<ul style="list-style-type: none; display:flex; padding: 0; justify-content: center;">
     @foreach ($medsos as $key => $data)
         <li style="margin: 4px">
             <a href="{{ $data->url }}" rel="noopener noreferrer" target="_blank">

c799a974f5b277ffcddae7f7c51c9cc8de795174
Nyoman Jyotisa <73281575+nyomanjyotisa@users.noreply.github.com>
2024-05-25 12:27:03 +0800

    refactor: tracking input field name value

--- app/Http/Controllers/SistemKomplain/SistemKomplainController.php
+++ app/Http/Controllers/SistemKomplain/SistemKomplainController.php
@@ -85,7 +85,7 @@ class SistemKomplainController extends Controller
     public function tracking(Request $request)
     {
         try {
-            $komplain = Komplain::where('komplain_id', '=', $request->post('q'))->firstOrFail();
+            $komplain = Komplain::where('komplain_id', '=', $request->post('tracking_id'))->firstOrFail();
             return redirect()->route('sistem-komplain.komplain', $komplain->slug);
         } catch (\Exception $e) {
             report($e);

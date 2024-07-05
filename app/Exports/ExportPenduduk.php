d4a3910422dd5e089591f9603ce2e84e2ade3b7c
Anang Novriadi <anangnov99@gmail.com>
2024-07-05 19:42:40 +0700

    fix elequent and title button

--- app/Exports/ExportPenduduk.php
+++ app/Exports/ExportPenduduk.php
@@ -18,25 +18,25 @@ class ExportPenduduk implements FromCollection, WithHeadings
     */
     public function collection()
     {
-        return DB::table('das_penduduk')
-            ->leftJoin('das_data_desa', 'das_penduduk.desa_id', '=', 'das_data_desa.desa_id')
-            ->leftJoin('ref_pendidikan_kk', 'das_penduduk.pendidikan_kk_id', '=', 'ref_pendidikan_kk.id')
-            ->leftJoin('ref_kawin', 'das_penduduk.status_kawin', '=', 'ref_kawin.id')
-            ->leftJoin('ref_pekerjaan', 'das_penduduk.pekerjaan_id', '=', 'ref_pekerjaan.id')
-            ->select([
-                'das_penduduk.id',
-                'das_penduduk.nama',
-                'das_penduduk.nik',
-                'das_penduduk.no_kk',
-                'das_data_desa.nama as nama_desa',
-                'das_penduduk.alamat',
-                'ref_pendidikan_kk.nama as pendidikan',
-                'das_penduduk.tanggal_lahir',
-                'ref_pekerjaan.nama as pekerjaan',
-                'ref_kawin.nama as status_kawin',
-            ])
-            ->where('status_dasar', 1)
-            ->get();
+        $data = [];
+        $penduduks = Penduduk::with('desa', 'pendidikan_kk', 'pekerjaan', 'kawin')->get();
+        
+        foreach ($penduduks as $penduduk) {
+            array_push($data, (object)[
+                'id' => $penduduk->id,
+                'nama' => $penduduk->nama,
+                'nik' => $penduduk->nik,
+                'no_kk' => $penduduk->no_kk,
+                'nama_desa' => $penduduk->desa->nama,
+                'alamat' => $penduduk->alamat,
+                'pendidikan' => $penduduk->pendidikan_kk->nama,
+                'tanggal_lahir' => $penduduk->tanggal_lahir,
+                'pekerjaan' => $penduduk->pekerjaan->nama,
+                'status_kawin' => $penduduk->pekerjaan->nama,
+            ]);
+        }
+
+        return collect($data);
     }
 
     /**

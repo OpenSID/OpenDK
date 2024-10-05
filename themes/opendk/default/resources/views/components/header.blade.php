207636aacdc29ce883693068fd06cd156e163310
misarianto <amikrian8@gmail.com>
2024-10-05 20:06:29 +0700

    ADD: melooping nav menu nested ke header publik dan membuat halaman berita per kategori di halaman publik ketika ada pembuatan menu nested type kategori

--- themes/opendk/default/resources/views/components/header.blade.php
+++ themes/opendk/default/resources/views/components/header.blade.php
@@ -14,7 +14,21 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
                 <ul class="nav navbar-nav">
-                    @foreach ($navigations as $nav)
+                    @foreach ($navmenus as $nav)
+                        @if ($nav['children']->count() > 0)
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"> {{ $nav['name'] }} <span class="caret"></span></a>
+                                <ul class="dropdown-menu fadeIn animated" role="menu">
+                                    @foreach ($nav['children'] as $child)
+                                        <li><a href="{{ url($child['url']) }}" target="{{ $child['target'] }}">{{ $child['name'] }}</a></li>                                   
+                                    @endforeach
+                                </ul>
+                            </li>
+                        @else
+                            <li><a href="{{ url($nav['url']) }}" target="{{ $nav['target'] }}">{{ $nav['name'] }}<span class="sr-only">(current)</span></a></li>
+                        @endif
+                    @endforeach
+                    {{-- @foreach ($navigations as $nav)
                         @if ($nav['childrens']->count() > 0)
                             <li class="dropdown">
                                 <a href="{{ $nav['full_url'] }}" class="dropdown-toggle" data-toggle="dropdown"> {{ $nav['name'] }} <span class="caret"></span></a>
@@ -27,7 +41,7 @@
                         @else
                             <li><a href="{{ url($nav['full_url']) }}">{{ $nav['name'] }}<span class="sr-only">(current)</span></a></li>
                         @endif
-                    @endforeach
+                    @endforeach --}}
 
                     @if (auth()->guest())
                         <li><a href="{{ route('login') }}">LogIn<span class="sr-only">(current)</span></a></li>

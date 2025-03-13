69cb698b154b1fe61b9457be3adaa358dbf0a01f
ahmad afandi <ahmad.afandi85@gmail.com>
2025-03-13 13:25:06 +0700

    Sesuaikan data statistik kependudukan berdasarkan pengaturan

---
+++ app/Services/DesaService.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * File ini bagian dari:
+ *
+ * OpenDK
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2017 - 2025 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+ *
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+ *
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package    OpenDK
+ * @author     Tim Pengembang OpenDesa
+ * @copyright  Hak Cipta 2017 - 2025 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license    http://www.gnu.org/licenses/gpl.html    GPL V3
+ * @link       https://github.com/OpenSID/opendk
+ */
+
+namespace App\Services;
+
+use App\Models\DataDesa;
+use Illuminate\Support\Facades\Cache;
+
+class DesaService extends BaseApiService
+{
+    public function dataDesa(string $slug)
+    {
+        if ($this->useDatabaseGabungan()) {
+            $slug = kembalikanSlug($slug);
+            $dataDesa = $this->desa(['filter[nama_desa]' => $slug, 'page[size]' => 1])?->first();
+            return $dataDesa;
+        }
+        return DataDesa::nama($slug)->firstOrFail();
+    }
+
+    public function listDesa()
+    {
+        if ($this->useDatabaseGabungan()) {
+            // gunakan cache untuk mempercepat load data melalui api
+            return Cache::remember('listDesa', 60 * 60 * 2, function () {
+                try {
+                    return $this->desa(['filter[kode_kec]' => config('profil.kecamatan_id')]);
+                } catch (\Exception $e) {                    
+                    \Log::error('Failed get data in '.__FILE__.' function minYear()'. $e->getMessage());
+                }
+                return collect();
+            });
+        }
+
+        return DataDesa::all();
+    }
+
+    /**
+     * Get Unique Desa
+     */
+    public function desa(array $filters = [])
+    {
+        // Panggil API dan ambil data
+        $data = $this->apiRequest('/api/v1/wilayah/desa', $filters);
+
+        return collect($data)->map(function ($item) {
+            return (object)[
+                'desa_id' => $item['attributes']['kode_desa'] ?? null, // Ambil kode desa
+                'kode_desa' => $item['attributes']['kode_desa'] ?? null, // Ambil kode desa
+                'nama' => $item['attributes']['nama_desa'] ?? null, // Ambil nama desa
+                'sebutan_desa' => $item['attributes']['sebutan_desa'] ?? null, // Ambil sebutan desa
+                'website' => $item['attributes']['website'] ?? null, // Ambil website
+                'luas_wilayah' => $item['attributes']['luas_wilayah'] ?? null, // Ambil luas wilayah
+                'path' => $item['attributes']['path'] ?? null, // Ambil path
+            ];
+        });
+    }
+}

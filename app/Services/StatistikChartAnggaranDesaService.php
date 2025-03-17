c0ebd6a4f41507224331d7e5891f3107a28b341e
ahmad afandi <ahmad.afandi85@gmail.com>
2025-03-17 08:03:01 +0700

    pending dulu

---
+++ app/Services/StatistikChartAnggaranDesaService.php
@@ -0,0 +1,140 @@
+<?php
+
+/*
+ * File ini bagian dari:
+ *
+ * OpenDK
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2017 - 2025 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+ *
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+ *
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package    OpenDK
+ * @author     Tim Pengembang OpenDesa
+ * @copyright  Hak Cipta 2017 - 2025 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license    http://www.gnu.org/licenses/gpl.html    GPL V3
+ * @link       https://github.com/OpenSID/opendk
+ */
+
+namespace App\Services;
+
+use App\Enums\LabelStatistik;
+use App\Models\CoaType;
+use Illuminate\Support\Facades\DB;
+
+class StatistikChartAnggaranDesaService extends BaseApiService
+{
+    public function chart($mid, $did, $year)
+    {
+        if ($this->useDatabaseGabungan()) {
+            $data = [];
+            try {
+                $filters = [
+                    'filter[id]' => 'agama',
+                    'filter[tahun]' => $year,
+                    'filter[kecamatan]' => config('profil.kecamatan_id'),
+                ];
+                if ($did != 'Semua') {
+                    $filters['filter[desa]'] = $did;
+                }
+                $response = $this->apiRequest('/api/v1/statistik/penduduk', $filters);
+                foreach ($response as $key => $item) {
+                    if (in_array($item['id'], [LabelStatistik::Total, LabelStatistik::Jumlah, LabelStatistik::BelumMengisi])) {
+                        continue;
+                    }
+                    $data[] = ['religion' => ucfirst(strtolower($item['attributes']['nama'])), 'total' => $item['attributes']['jumlah'], 'color' => $this->colors[$key] ?? '#'.random_color()];
+                }
+            } catch (\Exception $e) {
+                \Log::error('Failed get data in '.__FILE__.' function chart()'. $e->getMessage());
+            }
+            return $data;
+        }
+        return $this->localChart($mid, $did, $year);
+    }
+
+    private function localChart($mid, $did, $year)
+    {
+        // Grafik Data Anggaran
+        $dataAnggaran = [];
+        $type = CoaType::all();
+
+        if ($mid == 'Semua' && $year == 'Semua') {
+            $tmp = [];
+            foreach ($type as $val) {
+                $queryAnggaran = DB::table('das_anggaran_desa')->select('*')
+                    ->where('no_akun', 'LIKE', $val->id.'%');
+                if ($did != 'Semua') {
+                    $queryAnggaran->where('desa_id', $did);
+                }
+                $tmp[] = [
+                    'anggaran' => $val->id.' - '.$val->type_name,
+                    'jumlah' => $queryAnggaran->sum('jumlah'),
+                ];
+            }
+
+            $dataAnggaran['grafik'] = $tmp;
+        } elseif ($mid != 'Semua' && $year == 'Semua') {
+            $tmp = [];
+            foreach ($type as $val) {
+                $queryAnggaran = DB::table('das_anggaran_desa')->select('*')
+                    ->where('no_akun', 'LIKE', $val->id.'%')
+                    ->where('bulan', $mid);
+                if ($did != 'Semua') {
+                    $queryAnggaran->where('desa_id', $did);
+                }
+                $tmp[] = [
+                    'anggaran' => $val->id.' - '.$val->type_name,
+                    'jumlah' => $queryAnggaran->sum('jumlah'),
+                ];
+            }
+
+            $dataAnggaran['grafik'] = $tmp;
+        } elseif ($mid == 'Semua' && $year != 'Semua') {
+            $tmp = [];
+            foreach ($type as $val) {
+                $queryAnggaran = DB::table('das_anggaran_desa')->select('*')
+                    ->where('no_akun', 'LIKE', $val->id.'%')
+                    ->where('tahun', $year);
+                if ($did != 'Semua') {
+                    $queryAnggaran->where('desa_id', $did);
+                }
+                $tmp[] = [
+                    'anggaran' => $val->id.' - '.$val->type_name,
+                    'jumlah' => $queryAnggaran->sum('jumlah'),
+                ];
+            }
+            $dataAnggaran['grafik'] = $tmp;
+        } elseif ($mid != 'Semua' && $year != 'Semua') {
+            $tmp = [];
+            foreach ($type as $val) {
+                $queryAnggaran = DB::table('das_anggaran_desa')->select('*')
+                    ->where('no_akun', 'LIKE', $val->id.'%')
+                    ->where('bulan', $mid)
+                    ->where('tahun', $year);
+                if ($did != 'Semua') {
+                    $queryAnggaran->where('desa_id', $did);
+                }
+                $tmp[] = [
+                    'anggaran' => $val->id.' - '.$val->type_name,
+                    'jumlah' => $queryAnggaran->sum('jumlah'),
+                ];
+            }
+            $dataAnggaran['grafik'] = $tmp;
+        }
+
+        return $dataAnggaran;
+    }
+}

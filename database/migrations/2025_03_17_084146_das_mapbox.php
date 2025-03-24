df4ae40d4ce305ea4d8bad7f2c8168fcb69a006f
husnul <husnulseptia2209@gmail.com>
2025-03-24 10:05:17 +0700

    add models setting aplikasi

--- database/migrations/2025_03_17_084146_das_mapbox.php
+++ database/migrations/2025_03_17_084146_das_mapbox.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\SettingAplikasi;
 
 return new class extends Migration
 {

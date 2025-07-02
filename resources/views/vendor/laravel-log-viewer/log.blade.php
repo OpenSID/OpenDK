1cbe7e690842d91c29ae857b7d44c2f8e6cb32b4
pandigresik <pandigresik@users.noreply.github.com>
2025-07-02 07:05:44 +0000

    Blade Prettier Code

--- resources/views/vendor/laravel-log-viewer/log.blade.php
+++ resources/views/vendor/laravel-log-viewer/log.blade.php
@@ -24,15 +24,13 @@
             <div class="box-header with-border">
                 <h3 class="box-title"><i class="fa fa-folder" aria-hidden="true"></i>&nbsp;File Logs</h3>
                 <div class="box-tools">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-                            class="fa fa-minus"></i></button>
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                 </div>
             </div>
             <div class="box-body no-padding">
                 <ul class="nav nav-pills nav-stacked">
                     @foreach ($files as $file)
-                        <li @if ($current_file == $file) class="active" @endif><a
-                                href="?l={{ \Illuminate\Support\Facades\Crypt::encrypt($file) }}"> {{ $file }}
+                        <li @if ($current_file == $file) class="active" @endif><a href="?l={{ \Illuminate\Support\Facades\Crypt::encrypt($file) }}"> {{ $file }}
                             </a></li>
                     @endforeach
                 </ul>
@@ -45,23 +43,22 @@
                 <div class="p-3">
                     @if ($current_file)
                         <a class="btn btn-social btn-sm btn-success visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"
-                            href="?dl={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
+                            href="?dl={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}"
+                        >
                             <span class="fa fa-download"></span> Unduh
                         </a>
-                        <a class="btn btn-social btn-sm btn-info visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"
-                            id="clean-log"
-                            href="?clean={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
+                        <a class="btn btn-social btn-sm btn-info visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" id="clean-log"
+                            href="?clean={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}"
+                        >
                             <span class="fa fa-times-circle"></span> Bersihkan File
                         </a>
-                        <a class="btn btn-social btn-sm btn-danger visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"
-                            id="delete-log"
-                            href="?del={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
+                        <a class="btn btn-social btn-sm btn-danger visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" id="delete-log"
+                            href="?del={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}"
+                        >
                             <span class="fa fa-trash"></span> Hapus File
                         </a>
                         @if (count($files) > 1)
-                            <a id="delete-all-log"
-                                class="btn btn-social btn-sm btn-danger visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"
-                                href="?delall=true{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
+                            <a id="delete-all-log" class="btn btn-social btn-sm btn-danger visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" href="?delall=true{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
                                 <span class="fa fa-trash"></span> Hapus Semua file
                             </a>
                         @endif
@@ -74,8 +71,7 @@
                         <div>Log file >50M, Silahkan download.</div>
                     @else
                         <div class="table-responsive">
-                            <table id="table-log" class="table table-striped table table-bordered dataTable table-hover"
-                                data-ordering-index="{{ $standardFormat ? 2 : 0 }}">
+                            <table id="table-log" class="table table-striped table table-bordered dataTable table-hover" data-ordering-index="{{ $standardFormat ? 2 : 0 }}">
                                 <thead>
                                     <tr>
                                         @if ($standardFormat)
@@ -92,8 +88,7 @@
                                         <tr data-display="stack{{ $key }}">
                                             @if ($standardFormat)
                                                 <td class="nowrap text-{{ $log['level_class'] }}">
-                                                    <span class="fa fa-{{ $log['level_img'] }}"
-                                                        aria-hidden="true"></span>&nbsp;&nbsp;{{ $log['level'] }}
+                                                    <span class="fa fa-{{ $log['level_img'] }}" aria-hidden="true"></span>&nbsp;&nbsp;{{ $log['level'] }}
                                                 </td>
                                             @endif
                                             <td class="date">
@@ -101,9 +96,7 @@
                                             </td>
                                             <td class="text">
                                                 @if ($log['stack'])
-                                                    <button style="float:right" type="button"
-                                                        class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2"
-                                                        data-display="stack{{ $key }}">
+                                                    <button style="float:right" type="button" class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2" data-display="stack{{ $key }}">
                                                         <span class="fa fa-search"></span>
                                                     </button>
                                                 @endif
@@ -112,8 +105,7 @@
                                                     <br />{{ $log['in_file'] }}
                                                 @endif
                                                 @if ($log['stack'])
-                                                    <div class="stack" id="stack{{ $key }}"
-                                                        style="display: none; white-space: pre-wrap;">
+                                                    <div class="stack" id="stack{{ $key }}" style="display: none; white-space: pre-wrap;">
                                                         {{ trim($log['stack']) }}</div>
                                                 @endif
                                             </td>

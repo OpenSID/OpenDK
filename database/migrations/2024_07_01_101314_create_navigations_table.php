8cd8258924236bbc08c202813f8e6c8da9fb444c
ahmad afandi <ahmad.afandi85@gmail.com>
2025-07-10 10:52:11 +0700

    perbaikan migrasi awal

--- database/migrations/2024_07_01_101314_create_navigations_table.php
+++ database/migrations/2024_07_01_101314_create_navigations_table.php
@@ -7,7 +7,7 @@
  *
  * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
  *
- * Hak Cipta 2017 - 2024 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * Hak Cipta 2017 - 2025 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
  *
  * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
  * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
@@ -24,43 +24,39 @@
  *
  * @package    OpenDK
  * @author     Tim Pengembang OpenDesa
- * @copyright  Hak Cipta 2017 - 2024 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @copyright  Hak Cipta 2017 - 2025 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
  * @license    http://www.gnu.org/licenses/gpl.html    GPL V3
- * @
+ * @link       https://github.com/OpenSID/opendk
  */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
     public function up()
-{
-    if (!Schema::hasTable('das_navigation')) {
-        Schema::create('das_navigation', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('parent_id')->nullable();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->enum('nav_type', ['system', 'external'])->nullable();
-            $table->string('url')->nullable();
-            $table->integer('order')->nullable();
-            $table->tinyInteger('status')->default(0);
-            $table->integer('type')->nullable();
+    {
+        if (!Schema::hasTable('das_navigation')) {
+            Schema::create('das_navigation', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('parent_id')->nullable();
+                $table->string('name');
+                $table->string('slug')->unique();
+                $table->enum('nav_type', ['system', 'external'])->nullable();
+                $table->string('url')->nullable();
+                $table->integer('order')->nullable();
+                $table->tinyInteger('status')->default(0);
+                $table->integer('type')->nullable();
 
-            $table->timestamps();
-        });
-
-        Artisan::call('db:seed', ['--class' => 'DasNavigationTableSeeder']);
+                $table->timestamps();
+            });
+        }
     }
-}
-
 
     /**
      * Reverse the migrations.

49161499759f48fcb53c0a1213ea771c80ac8d00
Habibie <habibie.mahbub@gmail.com>
2025-08-14 16:48:16 +0700

    export tingkat pendidikan

---
+++ app/Exports/ExportTingkatPendidikan.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * File ini bagian dari:
+ *
+ * OpenDK
+ *
+ * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
+ *
+ * Hak Cipta 2017 - 2024 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ *
+ * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
+ * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
+ * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
+ * asal tunduk pada syarat berikut:
+ *
+ * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
+ * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
+ * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
+ *
+ * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
+ * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
+ * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
+ *
+ * @package    OpenDK
+ * @author     Tim Pengembang OpenDesa
+ * @copyright  Hak Cipta 2017 - 2024 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
+ * @license    http://www.gnu.org/licenses/gpl.html    GPL V3
+ * @link       https://github.com/OpenSID/opendk
+ */
+
+namespace App\Exports;
+
+use App\Models\TingkatPendidikan;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class ExportTingkatPendidikan implements FromCollection, WithHeadings, WithMapping, WithStyles
+{
+    /**
+     * Ambil koleksi data tingkat pendidikan.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function collection()
+    {
+        // Ambil semua data tingkat pendidikan dengan relasi desa
+        return TingkatPendidikan::with('desa')->get();
+    }
+
+    /**
+     * Definisi header kolom untuk export Excel.
+     *
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Nama Desa',
+            'Tidak Tamat Sekolah',
+            'Tamat SD',
+            'Tamat SMP',
+            'Tamat SMA',
+            'Tamat Diploma/Sederajat',
+            'Tahun',
+            'Semester',
+            'Tanggal Dibuat',
+            'Tanggal Diperbarui',
+        ];
+    }
+
+    /**
+     * Mapping data untuk setiap baris.
+     *
+     * @param $tingkatPendidikan
+     * @return array
+     */
+    public function map($tingkatPendidikan): array
+    {
+        return [
+            $tingkatPendidikan->id,
+            $tingkatPendidikan->desa->nama ?? '',
+            $tingkatPendidikan->tidak_tamat_sekolah,
+            $tingkatPendidikan->tamat_sd,
+            $tingkatPendidikan->tamat_smp,
+            $tingkatPendidikan->tamat_sma,
+            $tingkatPendidikan->tamat_diploma_sederajat,
+            $tingkatPendidikan->tahun,
+            $tingkatPendidikan->semester,
+            $tingkatPendidikan->created_at ? $tingkatPendidikan->created_at->format('d/m/Y H:i:s') : '',
+            $tingkatPendidikan->updated_at ? $tingkatPendidikan->updated_at->format('d/m/Y H:i:s') : '',
+        ];
+    }
+
+    /**
+     * Styling untuk Excel worksheet.
+     *
+     * @param Worksheet $sheet
+     * @return array
+     */
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style untuk header
+            1 => ['font' => ['bold' => true]],
+
+            // Auto-size untuk semua kolom
+            'A:K' => ['font' => ['size' => 10]],
+        ];
+    }
+}

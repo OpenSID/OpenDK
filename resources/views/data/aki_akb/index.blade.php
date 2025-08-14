34156b2dc321d54377d90ec96c95c84e00babba4
Habibie <habibie.mahbub@gmail.com>
2025-08-14 10:40:39 +0700

    ekspor aki akb

--- resources/views/data/aki_akb/index.blade.php
+++ resources/views/data/aki_akb/index.blade.php
@@ -1,52 +1,53 @@
 @extends('layouts.dashboard_template')
 
 @section('content')
-    <section class="content-header block-breadcrumb">
-        <h1>
-            {{ $page_title ?? 'Page Title' }}
-            <small>{{ $page_description ?? '' }}</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-            <li class="active">{{ $page_title }}</li>
-        </ol>
-    </section>
-    <section class="content container-fluid">
+<section class="content-header block-breadcrumb">
+    <h1>
+        {{ $page_title ?? 'Page Title' }}
+        <small>{{ $page_description ?? '' }}</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li class="active">{{ $page_title }}</li>
+    </ol>
+</section>
+<section class="content container-fluid">
 
-        @include('partials.flash_message')
+    @include('partials.flash_message')
 
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                @include('forms.btn-social', ['import_url' => route('data.aki-akb.import')])
-            </div>
-            <div class="box-body">
-                @include('layouts.fragments.list-desa')
-                <hr>
-                <div class="table-responsive">
-                    <table class="table table-bordered table-hover dataTable" id="aki-table">
-                        <thead>
-                            <tr>
-                                <th style="max-width: 100px;">Aksi</th>
-                                <th>{{ config('setting.sebutan_desa') }}</th>
-                                <th>Jumlah AKI</th>
-                                <th>Jumlah AKB</th>
-                                <th>Bulan</th>
-                                <th>Tahun</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
+    <div class="box box-primary">
+        <div class="box-header with-border">
+            @include('forms.btn-social', ['import_url' => route('data.aki-akb.import')])
+            @include('forms.btn-social', ['export_url' => route('data.aki-akb.export-excel')])
+        </div>
+        <div class="box-body">
+            @include('layouts.fragments.list-desa')
+            <hr>
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover dataTable" id="aki-table">
+                    <thead>
+                        <tr>
+                            <th style="max-width: 100px;">Aksi</th>
+                            <th>{{ config('setting.sebutan_desa') }}</th>
+                            <th>Jumlah AKI</th>
+                            <th>Jumlah AKB</th>
+                            <th>Bulan</th>
+                            <th>Tahun</th>
+                        </tr>
+                    </thead>
+                </table>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
 
 @include('partials.asset_select2')
 @include('partials.asset_datatables')
 
 @push('scripts')
-    <script type="text/javascript">
-        $(document).ready(function() {
+<script type="text/javascript">
+    $(document).ready(function() {
             var data = $('#aki-table').DataTable({
                 processing: true,
                 serverSide: true,
@@ -95,7 +96,7 @@
                 data.columns(1).search(this.value).draw();
             });
         });
-    </script>
-    @include('forms.datatable-vertical')
-    @include('forms.delete-modal')
+</script>
+@include('forms.datatable-vertical')
+@include('forms.delete-modal')
 @endpush

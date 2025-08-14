d2444ecdd8aafbdf9d946547cea953a936bb6324
habibie11 <habibie11@users.noreply.github.com>
2025-08-14 04:56:45 +0000

    Blade Prettier Code

--- resources/views/data/imunisasi/index.blade.php
+++ resources/views/data/imunisasi/index.blade.php
@@ -1,52 +1,52 @@
 @extends('layouts.dashboard_template')
 
 @section('content')
-<section class="content-header block-breadcrumb">
-    <h1>
-        {{ $page_title ?? 'Page Title' }}
-        <small>{{ $page_description ?? '' }}</small>
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-        <li class="active">{{ $page_title }}</li>
-    </ol>
-</section>
-<section class="content container-fluid">
+    <section class="content-header block-breadcrumb">
+        <h1>
+            {{ $page_title ?? 'Page Title' }}
+            <small>{{ $page_description ?? '' }}</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active">{{ $page_title }}</li>
+        </ol>
+    </section>
+    <section class="content container-fluid">
 
-    @include('partials.flash_message')
+        @include('partials.flash_message')
 
-    <div class="box box-primary">
-        <div class="box-header with-border">
-            @include('forms.btn-social', ['import_url' => route('data.imunisasi.import')])
-            @include('forms.btn-social', ['export_url' => route('data.imunisasi.export-excel')])
-        </div>
-        <div class="box-body">
-            @include('layouts.fragments.list-desa')
-            <hr>
-            <div class="table-responsive">
-                <table class="table table-bordered table-hover dataTable" id="imunisasi-table">
-                    <thead>
-                        <tr>
-                            <th style="max-width: 100px;">Aksi</th>
-                            <th>{{ config('setting.sebutan_desa') }}</th>
-                            <th>Cakupan Imunisasi</th>
-                            <th>Bulan</th>
-                            <th>Tahun</th>
-                        </tr>
-                    </thead>
-                </table>
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                @include('forms.btn-social', ['import_url' => route('data.imunisasi.import')])
+                @include('forms.btn-social', ['export_url' => route('data.imunisasi.export-excel')])
+            </div>
+            <div class="box-body">
+                @include('layouts.fragments.list-desa')
+                <hr>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover dataTable" id="imunisasi-table">
+                        <thead>
+                            <tr>
+                                <th style="max-width: 100px;">Aksi</th>
+                                <th>{{ config('setting.sebutan_desa') }}</th>
+                                <th>Cakupan Imunisasi</th>
+                                <th>Bulan</th>
+                                <th>Tahun</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 
 @include('partials.asset_select2')
 @include('partials.asset_datatables')
 
 @push('scripts')
-<script type="text/javascript">
-    $(document).ready(function() {
+    <script type="text/javascript">
+        $(document).ready(function() {
             var data = $('#imunisasi-table').DataTable({
                 processing: true,
                 serverSide: true,
@@ -89,7 +89,7 @@
                 data.columns(1).search(this.value).draw();
             });
         });
-</script>
-@include('forms.datatable-vertical')
-@include('forms.delete-modal')
+    </script>
+    @include('forms.datatable-vertical')
+    @include('forms.delete-modal')
 @endpush

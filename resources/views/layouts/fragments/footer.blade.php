c9257813b22127459871ddc287ad4995431ec9e6
habibie11 <habibie11@users.noreply.github.com>
2025-08-14 02:27:29 +0000

    Blade Prettier Code

--- resources/views/layouts/fragments/footer.blade.php
+++ resources/views/layouts/fragments/footer.blade.php
@@ -5,7 +5,6 @@
         <b><a href="https://github.com/openSID/openDK" target="_blank">OpenDK</a></b> {{ config('app.version') }}
     </div>
     <!-- Default to the left -->
-    <strong>Hak Cipta &copy; 2017 <a href="http://www.kompak.or.id">KOMPAK</a>, 2018-{{ date('Y') }} <a
-            href="http://opendesa.id">OpenDesa</a> <i class="fa fa-github"></i></strong>
+    <strong>Hak Cipta &copy; 2017 <a href="http://www.kompak.or.id">KOMPAK</a>, 2018-{{ date('Y') }} <a href="http://opendesa.id">OpenDesa</a> <i class="fa fa-github"></i></strong>
     Hak cipta dilindungi.
 </footer>

a0dab8c94b707a64d34ac2bbff4cb77fc5c4be4f
habibie11 <habibie11@users.noreply.github.com>
2025-08-15 02:53:12 +0000

    Blade Prettier Code

--- resources/views/data/anggaran_realisasi/index.blade.php
+++ resources/views/data/anggaran_realisasi/index.blade.php
@@ -1,51 +1,51 @@
 @extends('layouts.dashboard_template')
 
 @section('content')
-<section class="content-header block-breadcrumb">
-    <h1>
-        {{ $page_title ?? 'Page Title' }}
-        <small>{{ $page_description ?? '' }}</small>
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-        <li class="active">{{ $page_title }}</li>
-    </ol>
-</section>
-<section class="content container-fluid">
+    <section class="content-header block-breadcrumb">
+        <h1>
+            {{ $page_title ?? 'Page Title' }}
+            <small>{{ $page_description ?? '' }}</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active">{{ $page_title }}</li>
+        </ol>
+    </section>
+    <section class="content container-fluid">
 
-    @include('partials.flash_message')
+        @include('partials.flash_message')
 
-    <div class="box box-primary">
-        <div class="box-header with-border">
-            @include('forms.btn-social', ['import_url' => route('data.anggaran-realisasi.import')])
-            @include('forms.btn-social', ['export_url' => route('data.anggaran-realisasi.export-excel')])
-        </div>
-        <div class="box-body">
-            <div class="table-responsive">
-                <table class="table table-bordered table-hover dataTable" id="anggaran-table">
-                    <thead>
-                        <tr>
-                            <th style="max-width: 100px;">Aksi</th>
-                            <th>Total Anggaran</th>
-                            <th>Total Belanja</th>
-                            <th>Belanja Pegawai</th>
-                            <th>Belanja Barang & Jasa</th>
-                            <th>Belanja Modal</th>
-                            <th>Belanja Tidak Langsung</th>
-                            <th>Bulan</th>
-                            <th>Tahun</th>
-                        </tr>
-                    </thead>
-                </table>
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                @include('forms.btn-social', ['import_url' => route('data.anggaran-realisasi.import')])
+                @include('forms.btn-social', ['export_url' => route('data.anggaran-realisasi.export-excel')])
+            </div>
+            <div class="box-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover dataTable" id="anggaran-table">
+                        <thead>
+                            <tr>
+                                <th style="max-width: 100px;">Aksi</th>
+                                <th>Total Anggaran</th>
+                                <th>Total Belanja</th>
+                                <th>Belanja Pegawai</th>
+                                <th>Belanja Barang & Jasa</th>
+                                <th>Belanja Modal</th>
+                                <th>Belanja Tidak Langsung</th>
+                                <th>Bulan</th>
+                                <th>Tahun</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 @include('partials.asset_datatables')
 @push('scripts')
-<script type="text/javascript">
-    $(document).ready(function() {
+    <script type="text/javascript">
+        $(document).ready(function() {
             var data = $('#anggaran-table').DataTable({
                 processing: true,
                 serverSide: true,
@@ -95,7 +95,7 @@
                 ]
             });
         });
-</script>
-@include('forms.datatable-vertical')
-@include('forms.delete-modal')
+    </script>
+    @include('forms.datatable-vertical')
+    @include('forms.delete-modal')
 @endpush

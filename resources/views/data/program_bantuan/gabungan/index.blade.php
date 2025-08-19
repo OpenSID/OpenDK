3c81a260fb5dd0bac1fd59f8c8625b88a0ee8b9f
habibie11 <habibie11@users.noreply.github.com>
2025-08-19 04:01:30 +0000

    Blade Prettier Code

--- resources/views/data/program_bantuan/gabungan/index.blade.php
+++ resources/views/data/program_bantuan/gabungan/index.blade.php
@@ -1,66 +1,65 @@
 @extends('layouts.dashboard_template')
 
 @section('content')
-<section class="content-header block-breadcrumb">
-    <h1>
-        {{ $page_title ?? 'Page Title' }}
-        <small>{{ $page_description ?? '' }}</small>
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-        <li class="active">{{ $page_title }}</li>
-    </ol>
-</section>
-<section class="content container-fluid">
+    <section class="content-header block-breadcrumb">
+        <h1>
+            {{ $page_title ?? 'Page Title' }}
+            <small>{{ $page_description ?? '' }}</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active">{{ $page_title }}</li>
+        </ol>
+    </section>
+    <section class="content container-fluid">
 
-    @include('partials.flash_message')
+        @include('partials.flash_message')
 
-    <div class="box box-primary">
-        <div class="box-header with-border">
-            <a href="#">
-                <button type="button" id="export-excel-btn" class="btn btn-primary btn-sm btn-social"
-                    title="Export Excel">
-                    <i class="fa fa-download"></i>Export Excel
-                </button>
-            </a>
-        </div>
-        <div class="box-body">
-            <div class="row">
-                <div class="col-sm-3">
-                    <label>Desa</label>
-                    <select class="form-control" id="list_desa">
-                        <option value="">Semua Desa</option>
-                        @foreach ($list_desa as $desa)
-                        <option value="{{ $desa->kode_desa }}">{{ $desa->nama_desa }}</option>
-                        @endforeach
-                    </select>
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <a href="#">
+                    <button type="button" id="export-excel-btn" class="btn btn-primary btn-sm btn-social" title="Export Excel">
+                        <i class="fa fa-download"></i>Export Excel
+                    </button>
+                </a>
+            </div>
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <label>Desa</label>
+                        <select class="form-control" id="list_desa">
+                            <option value="">Semua Desa</option>
+                            @foreach ($list_desa as $desa)
+                                <option value="{{ $desa->kode_desa }}">{{ $desa->nama_desa }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <hr>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover dataTable" id="program-table">
+                        <thead>
+                            <tr>
+                                <th style="max-width: 150px;">Aksi</th>
+                                <th>Nama Program</th>
+                                <th>Desa</th>
+                                <th>Masa Berlaku</th>
+                                <th>Sasaran</th>
+                            </tr>
+                        </thead>
+                    </table>
                 </div>
             </div>
-            <hr>
-            <div class="table-responsive">
-                <table class="table table-bordered table-hover dataTable" id="program-table">
-                    <thead>
-                        <tr>
-                            <th style="max-width: 150px;">Aksi</th>
-                            <th>Nama Program</th>
-                            <th>Desa</th>
-                            <th>Masa Berlaku</th>
-                            <th>Sasaran</th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 @include('partials.asset_sweetalert')
 @include('partials.asset_select2')
 @include('partials.asset_datatables')
 
 @push('scripts')
-<script type="text/javascript">
-    $(document).ready(function() {
+    <script type="text/javascript">
+        $(document).ready(function() {
             function loadDesa() {
                 $.ajax({
                     url: `{{ $settings['api_server_database_gabungan'] ?? '' }}{{ '/api/v1/opendk/desa/' . str_replace('.', '', $profil->kecamatan_id) }}`,
@@ -187,7 +186,7 @@
                     const tableData = $('#program-table').DataTable();
                     const info = tableData.page.info();
                     const totalData = info.recordsTotal;
-                    
+
                     if (totalData === 0) {
                         Swal.fire({
                             icon: 'warning',
@@ -303,5 +302,5 @@
                 }
             }
         });
-</script>
+    </script>
 @endpush

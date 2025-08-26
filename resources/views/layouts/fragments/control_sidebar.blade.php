47a55dc9db6e9683a1630ac5427f74251d56a587
pandigresik <pandigresik@users.noreply.github.com>
2025-08-26 04:28:33 +0000

    Blade Prettier Code

--- resources/views/layouts/fragments/control_sidebar.blade.php
+++ resources/views/layouts/fragments/control_sidebar.blade.php
@@ -14,8 +14,7 @@
             <p>Jika Anda butuh bantuan untuk menggunakan Aplikasi Dashboard Kecamatan, silakan Anda Unduh Panduan
                 Pengguna di bawah ini.</p>
             <br>
-            <a href="{{ asset('storage/template_upload/Panduan_Pengguna_Kecamatan_Dashboard.pdf') }}" target="_blank"
-                class="btn btn-primary btn-lg col-md-12"><i class="fa fa-download"></i> Unduh Panduan</a>
+            <a href="{{ asset('storage/template_upload/Panduan_Pengguna_Kecamatan_Dashboard.pdf') }}" target="_blank" class="btn btn-primary btn-lg col-md-12"><i class="fa fa-download"></i> Unduh Panduan</a>
             <br>
             <br>
         </div>

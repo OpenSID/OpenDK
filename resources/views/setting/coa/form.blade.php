6faf1a3c71b8bc40e645229453368d63c396068a
Habibie <habibie.mahbub@gmail.com>
2025-08-28 16:17:22 +0700

    sesuaikan 5

--- resources/views/setting/coa/form.blade.php
+++ resources/views/setting/coa/form.blade.php
@@ -1,32 +1,35 @@
 <div class="form-group">
-    <label for="type_id" class="control-label col-md-3 col-sm-6 col-xs-12">Tipe Akun <span class="required">*</span></label>
+    <label for="type_id" class="control-label col-md-3 col-sm-6 col-xs-12">Tipe Akun <span
+            class="required">*</span></label>
 
     <div class="col-md-3 col-sm-4 col-xs-12">
         <select name="type_id" class="form-control" id="type_id">
             @foreach (\App\Models\CoaType::all() as $type)
-                <option value="{{ $type->id }}">{{ $type->id . ' - ' . $type->type_name }}</option>
+            <option value="{{ $type->id }}">{{ $type->id . ' - ' . $type->type_name }}</option>
             @endforeach
         </select>
     </div>
 </div>
 <div class="form-group">
-    <label for="sub_id" class="control-label col-md-3 col-sm-6 col-xs-12">Sub Akun <span class="required">*</span></label>
+    <label for="sub_id" class="control-label col-md-3 col-sm-6 col-xs-12">Sub Akun <span
+            class="required">*</span></label>
 
     <div class="col-md-3 col-sm-4 col-xs-12">
         <select name="sub_id" class="form-control" id="sub_id">
             @foreach (\App\Models\SubCoa::where('type_id', 4)->get() as $sub)
-                <option value="{{ $sub->id }}">{{ $sub->id . ' - ' . $sub->sub_name }}</option>
+            <option value="{{ $sub->id }}">{{ $sub->id . ' - ' . $sub->sub_name }}</option>
             @endforeach
         </select>
     </div>
 </div>
 <div class="form-group">
-    <label for="sub_sub_id" class="control-label col-md-3 col-sm-6 col-xs-12">Sub Sub Akun <span class="required">*</span></label>
+    <label for="sub_sub_id" class="control-label col-md-3 col-sm-6 col-xs-12">Sub Sub Akun <span
+            class="required">*</span></label>
 
     <div class="col-md-3 col-sm-4 col-xs-12">
         <select name="sub_sub_id" class="form-control" id="sub_sub_id">
             @foreach (\App\Models\SubSubCoa::where('type_id', 4)->where('sub_id', 1)->get() as $sub_sub)
-                <option value="{{ $sub_sub->id }}">{{ $sub_sub->id . ' - ' . $sub_sub->sub_sub_name }}</option>
+            <option value="{{ $sub_sub->id }}">{{ $sub_sub->id . ' - ' . $sub_sub->sub_sub_name }}</option>
             @endforeach
         </select>
     </div>
@@ -35,22 +38,24 @@
     <label for="id" class="control-label col-md-3 col-sm-6 col-xs-12">ID Akun <span class="required">*</span></label>
 
     <div class="col-md-3 col-sm-6 col-xs-12">
-        {!! Form::text('id', null, ['class' => 'form-control', 'required' => true, 'id' => 'id', 'readonly' => true]) !!}
+        {!! Html::text('id', null, ['class' => 'form-control', 'required' => true, 'id' => 'id', 'readonly' => true])
+        !!}
     </div>
 </div>
 <div class="form-group">
-    <label for="coa_name" class="control-label col-md-3 col-sm-6 col-xs-12">Nama Akun <span class="required">*</span></label>
+    <label for="coa_name" class="control-label col-md-3 col-sm-6 col-xs-12">Nama Akun <span
+            class="required">*</span></label>
 
     <div class="col-md-8 col-sm-6 col-xs-12">
-        {!! Form::text('coa_name', null, ['class' => 'form-control', 'required' => true, 'id' => 'coa_name']) !!}
+        {!! Html::text('coa_name', null, ['class' => 'form-control', 'required' => true, 'id' => 'coa_name']) !!}
     </div>
 </div>
 <div class="ln_solid"></div>
 @include('partials.asset_datatables')
 
 @push('scripts')
-    <script type="text/javascript">
-        $(document).ready(function() {
+<script type="text/javascript">
+    $(document).ready(function() {
 
             $('#type_id').on('change', function(e) {
                 var type_id = this.value;
@@ -106,7 +111,7 @@
             var sub_sub_id = $('#sub_sub_id').val();
             generate_id(type_id, sub_id, sub_sub_id);
         });
-    </script>
-    @include('forms.datatable-vertical')
-    @include('forms.delete-modal')
+</script>
+@include('forms.datatable-vertical')
+@include('forms.delete-modal')
 @endpush
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,53 +1,53 @@
 @extends('layouts.dashboard_template')
 
 @section('content')
-    <!-- Content Header (Page header) -->
-    <section class="content-header block-breadcrumb">
-        <h1>
-            {{ $page_title ?? 'Page Title' }}
-            <small>{{ $page_description ?? '' }}</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-            <li><a href="{{ route('setting.role.index') }}">{{ $page_title }}</a></li>
-            <li class="active">{{ $page_description }}</li>
-        </ol>
-    </section>
-    <section class="content">
-        <div class="box box-primary">
-            <div class="box-body">
-                @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        <strong>Ups!</strong> Ada beberapa masalah dengan masukan Anda.<br><br>
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-                {!! Form::open([
-                    'route' => 'setting.user.store',
-                    'method' => 'post',
-                    'files' => true,
-                    'id' => 'form-user',
-                    'class' => 'form-horizontal form-label-left',
-                ]) !!}
-                @include('flash::message')
-                @include('user.form')
-                {!! Form::close() !!}
+<!-- Content Header (Page header) -->
+<section class="content-header block-breadcrumb">
+    <h1>
+        {{ $page_title ?? 'Page Title' }}
+        <small>{{ $page_description ?? '' }}</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="{{ route('setting.role.index') }}">{{ $page_title }}</a></li>
+        <li class="active">{{ $page_description }}</li>
+    </ol>
+</section>
+<section class="content">
+    <div class="box box-primary">
+        <div class="box-body">
+            @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Ups!</strong> Ada beberapa masalah dengan masukan Anda.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
+            @endif
+            {!! Html::form()
+            ->route('setting.user.store')
+            ->method('post')
+            ->attribute('files', true)
+            ->id('form-user')
+            ->class('form-horizontal form-label-left')
+            ->open() !!}
+            @include('flash::message')
+            @include('user.form')
+            {!! Html::closeForm() !!}
         </div>
-    </section>
+    </div>
+</section>
 @endsection
 
 @push('scripts')
-    {!! JsValidator::formRequest('App\Http\Requests\UserRequest', '#form-user') !!}
-    <script type="text/javascript">
-        $('.showpass').hover(function() {
+{!! JsValidator::formRequest('App\Http\Requests\UserRequest', '#form-user') !!}
+<script type="text/javascript">
+    $('.showpass').hover(function() {
             $('.password').attr('type', 'text');
         }, function() {
             $('.password').attr('type', 'password');
         });
-    </script>
+</script>
 @endpush

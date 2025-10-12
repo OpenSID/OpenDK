c40ff47b0a3e4e7239c64a4a8e418312844b2a31
Habibie <habibie.mahbub@gmail.com>
2025-10-12 17:38:00 +0700

    cast to boolean

--- app/Models/User.php
+++ app/Models/User.php
@@ -107,6 +107,7 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
         'otp_enabled' => 'boolean',
+        'two_fa_enabled' => 'boolean',
     ];
 
     /**

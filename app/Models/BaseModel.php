e6e019dfa807d3364e7a2731efacb15a8d7f355a
Ahmad Afandi <ahmad.afandi85@gmail.com>
2025-11-27 08:20:54 +0700

    update file .env ketika profil diupdate

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use App\Traits\HasTenantScope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BaseModel extends Model
 {
     use HasTenantScope;
+
+    public function tenant(){
+        return $this->belongsTo(Tenant::class);
+    }
 }

b1ea400d330af9ef701d817a9a87091faeb05657
Habibie Mahbub <56639378+habibie11@users.noreply.github.com>
2025-12-12 11:35:46 +0700

    Penyesuaian upgrade package (#1385)
    Co-authored-by: Ahmad Afandi <ahmad.afandi85@gmail.com>
    Co-authored-by: Abah Roland <v.rolanda92@gmail.com>

--- resources/views/livewire/informasi/media_terkait/form.blade.php
+++ resources/views/livewire/informasi/media_terkait/form.blade.php
@@ -10,48 +10,43 @@
             <form class="form-horizontal" enctype="multipart/form-data">
                 <div class="box-body">
 
-                    <x-form-group name="instance.nama">
-                        <x-label for="instance.nama">Nama</x-label>
+                    <x-form-group name="media_terkait.nama">
+                        <x-label for="media_terkait.nama">Nama</x-label>
                         <div class="col-sm-8">
-                            <x-input type="text" placeholder="Nama Media Terkait" wire:model="instance.nama" />
-                            <x-input-validation name="instance.nama" />
+                            <x-input type="text" placeholder="Nama Media Terkait"
+                                wire:model.blur="media_terkait.nama" />
+                            <x-input-validation name="media_terkait.nama" />
                         </div>
                     </x-form-group>
 
-                    <x-form-group name="instance.url">
-                        <x-label for="instance.url">Link</x-label>
+                    <x-form-group name="media_terkait.url">
+                        <x-label for="media_terkait.url">Link</x-label>
                         <div class="col-sm-8">
-                            <x-input type="text" placeholder="Link Media Terkait" wire:model="instance.url" />
-                            <x-input-validation name="instance.url" />
+                            <x-input type="text" placeholder="Link Media Terkait"
+                                wire:model.blur="media_terkait.url" />
+                            <x-input-validation name="media_terkait.url" />
                         </div>
                     </x-form-group>
 
                     <x-form-group name="logo">
                         <x-label for="logo">Logo</x-label>
                         <div class="col-sm-8">
-                            <x-filepond
-                                wire:model="logo"
-                                allowImagePreview
-                                imagePreviewMaxHeight="200"
-                                allowFileTypeValidation
-                                acceptedFileTypes="['image/png', 'image/jpg', 'image/jpeg']"
-                                allowFileSizeValidation
-                                maxFileSize="4mb"
-                                :existingFile="$logo"
-                            />
+                            <x-filepond wire:model="logo" allowImagePreview imagePreviewMaxHeight="200"
+                                allowFileTypeValidation acceptedFileTypes="['image/png', 'image/jpg', 'image/jpeg']"
+                                allowFileSizeValidation maxFileSize="4mb" :existingFile="$logo" />
                         </div>
                     </x-form-group>
 
-                    <x-form-group name="instance.status">
-                        <x-label for="instance.status">Status</x-label>
+                    <x-form-group name="media_terkait.status">
+                        <x-label for="media_terkait.status">Status</x-label>
                         <div class="col-sm-8">
-                            <x-tom-select wire:model="instance.status">
+                            <x-tom-select wire:model="media_terkait.status">
                                 <option value="">-- Pilih Status --</option>
                                 <option value="1">Tampil</option>
                                 <option value="0">Tidak Tampil</option>
                             </x-tom-select>
 
-                            <x-input-validation name="instance.status" />
+                            <x-input-validation name="media_terkait.status" />
                         </div>
                     </x-form-group>
                 </div>
@@ -60,7 +55,10 @@
                         <i class="fa fa-times"></i> Reset
                     </x-button>
 
-                    <button type="button" class="btn btn-info btn-sm" wire:click.prevent="{{ $editMode ? 'update' : 'store' }}" @if (empty($logo)) disabled @endif><i class="fa fa-save"></i> {{ $editMode ? 'Update' : 'Simpan' }}</button>
+                    <x-button color="info" wire:click.prevent="{{ $editMode ? 'update' : 'store' }}"
+                        loading="{{ $editMode ? 'Update' : 'Simpan' }}...">
+                        <i class="fa fa-save"></i> {{ $editMode ? 'Update' : 'Simpan' }}
+                    </x-button>
 
                 </div>
             </form>

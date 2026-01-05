664fa8990ba583759922edc574dfa5dda7b9bbf4
habibie11 <habibie.mahbub@gmail.com>
2026-01-05 10:06:24 +0700

    fix

---
+++ database/migrations/2026_01_05_012917_set_nullable_das.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('das_profil', function (Blueprint $table) {
+            $table->string('kabupaten_id', 5)->nullable()->change();
+            $table->string('dasar_pembentukan', 50)->nullable()->change();
+        });
+
+        Schema::table('das_data_umum', function (Blueprint $table) {
+            $table->longText('tipologi')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('das_profil', function (Blueprint $table) {
+            $table->string('kabupaten_id', 5)->nullable(false)->change();
+            $table->string('dasar_pembentukan', 50)->nullable(false)->change();
+        });
+
+        Schema::table('das_data_umum', function (Blueprint $table) {
+            $table->longText('tipologi')->nullable(false)->change();
+        });
+    }
+};

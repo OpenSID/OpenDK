eff75e18f525971b90fb074647cfd1f43fbcfb41
habibie11 <habibie.mahbub@gmail.com>
2026-04-01 11:26:19 +0700

    fix blade prettier

--- resources/views/data/lembaga_anggota/edit.blade.php
+++ resources/views/data/lembaga_anggota/edit.blade.php
@@ -20,8 +20,12 @@
         <div class="col-md-12">
             <div class="box box-primary">
 
-                {!! html()->form('PUT', route('data.lembaga_anggota.update', $lembaga->slug,
-                $anggota->id))->id('form-lembaga-anggota')->class('form-horizontal form-label-left')->open() !!}
+                {{-- @dd($anggota) --}}
+
+                {!! html()->form('PUT', route('data.lembaga_anggota.update', [
+                    'id' => $anggota->id,
+                    'slug' => $lembaga->slug,
+                ]))->id('form-lembaga-anggota')->class('form-horizontal form-label-left')->open() !!}
 
                 @include('layouts.fragments.error_message')
 

b14aa53a7e7eb3c4dca57d99c443fc951e220a64
pandigresik <pandigresik@users.noreply.github.com>
2026-04-15 05:51:57 +0000

    Blade Prettier Code

--- resources/views/vendor/laravel-log-viewer/info-sistem.blade.php
+++ resources/views/vendor/laravel-log-viewer/info-sistem.blade.php
@@ -18,59 +18,65 @@
 </div>
 
 <style>
-#phpinfo-content {
-    max-height: 600px;
-    overflow-y: auto;
-}
-#phpinfo-content pre {
-    margin: 0;
-    padding: 0;
-    background: transparent;
-    border: none;
-}
-#phpinfo-content table {
-    width: 100%;
-    border-collapse: collapse;
-}
-#phpinfo-content table td, 
-#phpinfo-content table th {
-    padding: 5px 10px;
-    border: 1px solid #ddd;
-}
-#phpinfo-content .e {
-    background-color: #f0f0f0;
-    font-weight: bold;
-}
-#phpinfo-content .h {
-    background-color: #e8f0fe;
-}
-#phpinfo-content center {
-    display: none;
-}
+    #phpinfo-content {
+        max-height: 600px;
+        overflow-y: auto;
+    }
+
+    #phpinfo-content pre {
+        margin: 0;
+        padding: 0;
+        background: transparent;
+        border: none;
+    }
+
+    #phpinfo-content table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    #phpinfo-content table td,
+    #phpinfo-content table th {
+        padding: 5px 10px;
+        border: 1px solid #ddd;
+    }
+
+    #phpinfo-content .e {
+        background-color: #f0f0f0;
+        font-weight: bold;
+    }
+
+    #phpinfo-content .h {
+        background-color: #e8f0fe;
+    }
+
+    #phpinfo-content center {
+        display: none;
+    }
 </style>
 
 @push('scripts')
-<script>
-$(document).on('click', '#load-phpinfo', function() {
-    var btn = $(this);
-    var container = $('#phpinfo-content');
-    
-    btn.prop('disabled', true).html('<i class="fa fa-spinner fa-spin"></i> Memuat...');
-    
-    $.ajax({
-        url: '{{ route("setting.info-sistem.phpinfo") }}',
-        type: 'GET',
-        success: function(response) {
-            container.show().html(response);
-            btn.hide();
-            
-            container.find('table').addClass('table table-bordered');
-        },
-        error: function(xhr) {
-            container.show().html('<div class="alert alert-danger">Gagal memuat info sistem</div>');
-            btn.prop('disabled', false).html('<i class="fa fa-refresh"></i> Tampilkan Info Sistem');
-        }
-    });
-});
-</script>
+    <script>
+        $(document).on('click', '#load-phpinfo', function() {
+            var btn = $(this);
+            var container = $('#phpinfo-content');
+
+            btn.prop('disabled', true).html('<i class="fa fa-spinner fa-spin"></i> Memuat...');
+
+            $.ajax({
+                url: '{{ route('setting.info-sistem.phpinfo') }}',
+                type: 'GET',
+                success: function(response) {
+                    container.show().html(response);
+                    btn.hide();
+
+                    container.find('table').addClass('table table-bordered');
+                },
+                error: function(xhr) {
+                    container.show().html('<div class="alert alert-danger">Gagal memuat info sistem</div>');
+                    btn.prop('disabled', false).html('<i class="fa fa-refresh"></i> Tampilkan Info Sistem');
+                }
+            });
+        });
+    </script>
 @endpush

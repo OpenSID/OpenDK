16da5655882c5f379ca56239b91e673297dca147
habibie11 <habibie.mahbub@gmail.com>
2026-05-11 22:21:48 +0700

    ubah method post di data finansial

--- resources/views/data/anggaran_desa/gabungan/index.blade.php
+++ resources/views/data/anggaran_desa/gabungan/index.blade.php
@@ -57,15 +57,15 @@
                 processing: true,
                 serverSide: true,
                 ajax: {
-                    url: `{{ $settings['api_server_database_gabungan'] ?? '' }}{{ '/api/v1/keuangan/apbdes?' .
+                    url: `{{ $settings['api_server_database_gabungan'] ?? '' }}{{ '/api/v1/keuangan/apbdes-datatable?' .
                         http_build_query([
                             'filter[kode_kecamatan]' => str_replace('.', '', $profil->kecamatan_id),
                         ]) }}`,
                     headers: {
-                        "Accept": "application/ld+json",
-                        "Content-Type": "text/json; charset=utf-8",
-                        "Authorization": `Bearer {{ $settings['api_key_database_gabungan'] ?? '' }}`
+                        "Authorization": `Bearer {{ $settings['api_key_database_gabungan'] ?? '' }}`,
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                     },
+                    method: 'POST',
                     data: function(row) {
                         return {
                             "page[size]": row.length,
--- resources/views/data/laporan-apbdes/gabungan/index.blade.php
+++ resources/views/data/laporan-apbdes/gabungan/index.blade.php
@@ -58,15 +58,17 @@
                 processing: true,
                 serverSide: true,
                 ajax: {
-                    url: `{{ $settings['api_server_database_gabungan'] ?? '' }}{{ '/api/v1/keuangan/laporan_apbdes?' .
+                    url: `{{ $settings['api_server_database_gabungan'] ?? '' }}{{ '/api/v1/keuangan/laporan_apbdes-datatable?' .
                         http_build_query([
                             'filter[kode_kecamatan]' => str_replace('.', '', $profil->kecamatan_id),
                         ]) }}`,
                     headers: {
                         "Accept": "application/ld+json",
                         "Content-Type": "text/json; charset=utf-8",
-                        "Authorization": `Bearer {{ $settings['api_key_database_gabungan'] ?? '' }}`
+                        "Authorization": `Bearer {{ $settings['api_key_database_gabungan'] ?? '' }}`,
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                     },
+                    method: 'POST',
                     data: function(row) {
                         return {
                             "page[size]": row.length,

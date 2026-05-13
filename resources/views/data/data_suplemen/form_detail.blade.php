3cf39f55cd935dea1c2c220d1b4df056c0fb77c4
Habibie Mahbub <56639378+habibie11@users.noreply.github.com>
2026-05-13 15:40:32 +0700

    Merge Pull Request From Fix: WAF Blocking pada DataTables Menu Data → Kecamatan (GET ke POST) (#1516)
    Co-authored-by: habibie11 <habibie11@users.noreply.github.com>
    Co-authored-by: Ahmad Affandi <caspianp98@yahoo.com>

--- resources/views/data/data_suplemen/form_detail.blade.php
+++ resources/views/data/data_suplemen/form_detail.blade.php
@@ -26,7 +26,9 @@
                     @if ($anggota == null)
                         <option value="{{ $penduduk['id'] }}">{{ $penduduk['nama'] }}</option>
                     @else
-                        <option {{ $anggota->penduduk->id == $penduduk['id'] ? 'selected' : '' }} value="{{ $penduduk['id'] }}">{{ $penduduk['nama'] }}</option>
+                        <option {{ $anggota->penduduk->id == $penduduk['id'] ? 'selected' : '' }} value="{{ $penduduk['id'] }}">
+                            {{ $penduduk['nama'] }}
+                        </option>
                     @endif
                 @endforeach
             </select>
@@ -54,7 +56,7 @@
         <label class="control-label col-md-3 col-sm-3 col-xs-12"
             for="penduduk_id">{{ $suplemen->sasaran == 2
                 ? 'Nama Kepala
-                                                                                                                                                                                    Keluarga'
+                                                                                                                                                                                                    Keluarga'
                 : 'Nama Penduduk' }}</label>
 
         <div class="col-md-6 col-sm-6 col-xs-12">
@@ -64,7 +66,9 @@
                     @if ($anggota == null)
                         <option value="{{ $penduduk['id'] }}">{{ $penduduk['nama'] }}</option>
                     @else
-                        <option {{ $anggota->penduduk->id == $penduduk['id'] ? 'selected' : '' }} value="{{ $penduduk['id'] }}">{{ $penduduk['nama'] }}</option>
+                        <option {{ $anggota->penduduk->id == $penduduk['id'] ? 'selected' : '' }} value="{{ $penduduk['id'] }}">
+                            {{ $penduduk['nama'] }}
+                        </option>
                     @endif
                 @endforeach
             </select>

d1280644ad476a4139c35a75db3f1f9159adad83
habibie11 <habibie.mahbub@gmail.com>
2026-06-24 16:35:19 +0700

    fix: generate token sinkronisasi

--- app/Http/Controllers/Api/TokenController.php
+++ app/Http/Controllers/Api/TokenController.php
@@ -32,6 +32,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\SettingAplikasi;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -49,6 +50,12 @@ class TokenController extends Controller
         Config::set('jwt.ttl', 10 * 365 * 24 * 60);
         $user = Auth::user();
         $token = JWTAuth::fromUser($user);
+
+        // Simpan token ke das_setting agar middleware token.registered dapat memvalidasinya
+        SettingAplikasi::updateOrCreate(
+            ['key' => 'api_key_opendk'],
+            ['value' => $token]
+        );
 
         // Return the token in a response
         return response()->json(['token' => $token]);

b98352b1ee81c653661bd0c29bf203b438c54ad5
habibie11 <habibie.mahbub@gmail.com>
2026-06-29 09:35:47 +0700

    sesuaikan sinkron pembangunan

--- app/Imports/SinkronPembangunan.php
+++ app/Imports/SinkronPembangunan.php
@@ -39,7 +39,7 @@ use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class SinkronPembangunan implements ToCollection, WithHeadingRow, WithChunkReading, ShouldQueue
+class SinkronPembangunan implements ShouldQueue, ToCollection, WithChunkReading, WithHeadingRow
 {
     use Importable;
 

50c54e4e3a2113618d0dd786bf33a32437dd0851
Ahmad Afandi <ahmad.afandi85@gmail.com>
2026-07-10 06:41:25 +0700

    perbaikan button

--- resources/views/components/action-dropdown.blade.php
+++ resources/views/components/action-dropdown.blade.php
@@ -2,7 +2,7 @@
     <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="Aksi">
         <i class="fa fa-list"></i> Proses <span class="caret"></span>
     </button>
-    <ul class="dropdown-menu dropdown-menu-left" style="position: absolute; top: 100%; top: -8px ; left: 90px; z-index: 1050;">
-        {!! $slot !!}
+    <ul class="dropdown-menu dropdown-menu-left">
+        {{ $slot }}
     </ul>
 </div>

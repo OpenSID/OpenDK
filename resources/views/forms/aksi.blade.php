1bc8557a1b7149010d47dbf248baae457a14eec0
Ahmad Afandi <ahmad.afandi85@gmail.com>
2026-07-10 08:24:46 +0700

    perbaikan smoke test

--- resources/views/forms/aksi.blade.php
+++ resources/views/forms/aksi.blade.php
@@ -3,10 +3,10 @@
         <li><a href="{!! $show_web !!}" target="_blank" title="Selengkapnya"><i class="fa fa-eye text-warning"></i> Selengkapnya</a></li>
     @endif
     @if (!empty($detail_url))
-        <li><a href="{!! $detail_url !!}" title="Selengkapnya"><i class="fa fa-list text-info"></i> Selengkapnya</a></li>
+        <li><a href="{!! $detail_url !!}" title="Selengkapnya" data-testid="btn-lihat"><i class="fa fa-list text-info"></i> Selengkapnya</a></li>
     @endif
     @if (!empty($show_url))
-        <li><a href="{!! $show_url !!}" title="Lihat" target="_blank"><i class="fa fa-eye text-warning"></i> Lihat</a></li>
+        <li><a href="{!! $show_url !!}" title="Lihat" target="_blank" data-testid="btn-lihat"><i class="fa fa-eye text-warning"></i> Lihat</a></li>
     @endif
     @if (!empty($peta))
         <li><a href="{!! $peta !!}" title="Peta" target="_blank"><i class="fa fa-map text-info"></i> Peta</a></li>
@@ -20,7 +20,7 @@
     @endif
 
     @if (!empty($edit_url))
-        <li><a href="{!! $edit_url !!}" title="Ubah"><i class="fa fa-edit text-success"></i> Ubah</a></li>
+        <li><a href="{!! $edit_url !!}" title="Ubah" data-testid="btn-edit"><i class="fa fa-edit text-success"></i> Ubah</a></li>
     @endif
     @if (!empty($modal_form))
         <li><a href="javascript:void(0)" class="open_form" data-id="{{ $modal_form }}" title="Ubah"><i class="fa fa-edit text-warning"></i> Ubah</a></li>
@@ -30,7 +30,7 @@
     @endif
 
     @if (!empty($download_url))
-        <li><a href="{!! $download_url !!}" title="Unduh"><i class="fa fa-download text-info"></i> Unduh</a></li>
+        <li><a href="{!! $download_url !!}" title="Unduh" data-testid="btn-download"><i class="fa fa-download text-info"></i> Unduh</a></li>
     @endif
     @if (!empty($download_zip))
         <li><a href="{!! $download_zip !!}" title="Unduh: {{ $nama_file ?? 'Arsip' }}"><i class="glyphicon glyphicon-download-alt text-primary"></i> Unduh ZIP</a></li>
@@ -44,10 +44,10 @@
     @endif
 
     @if (!empty($lock_url))
-        <li><a href="javascript:void(0)" data-href="{!! $lock_url !!}" id="lockModal" title="Tidak Aktif"><i class="fa fa-lock text-muted"></i> Nonaktifkan</a></li>
+        <li><a href="javascript:void(0)" data-href="{!! $lock_url !!}" id="lockModal" title="Tidak Aktif" data-testid="btn-lock"><i class="fa fa-lock text-muted"></i> Nonaktifkan</a></li>
     @endif
     @if (!empty($unlock_url))
-        <li><a href="javascript:void(0)" data-href="{!! $unlock_url !!}" id="unlockModal" title="Aktif"><i class="fa fa-unlock text-success"></i> Aktifkan</a></li>
+        <li><a href="javascript:void(0)" data-href="{!! $unlock_url !!}" id="unlockModal" title="Aktif" data-testid="btn-unlock"><i class="fa fa-unlock text-success"></i> Aktifkan</a></li>
     @endif
     @if (!empty($active_url))
         <li><a href="javascript:void(0)" data-href="{!! $active_url !!}" id="activeModal" title="Aktif"><i class="fa fa-check text-success"></i> Aktifkan</a></li>
@@ -70,6 +70,6 @@
 
     @if (!empty($delete_url))
         <li class="divider"></li>
-        <li><a href="javascript:void(0)" data-href="{!! $delete_url !!}" id="deleteModal" title="Hapus" style="color: #dd4b39;"><i class="fa fa-trash text-danger"></i> Hapus</a></li>
+        <li><a href="javascript:void(0)" data-href="{!! $delete_url !!}" id="deleteModal" title="Hapus" style="color: #dd4b39;" data-testid="btn-hapus"><i class="fa fa-trash text-danger"></i> Hapus</a></li>
     @endif
 </x-action-dropdown>

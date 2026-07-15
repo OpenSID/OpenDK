13f50da49848aede76aae54a618fe8824b25b206
Ahmad Afandi <ahmad.afandi85@gmail.com>
2026-07-15 14:44:16 +0700

    perbaiki lebar kolom

--- resources/views/informasi/prosedur/index.blade.php
+++ resources/views/informasi/prosedur/index.blade.php
@@ -24,7 +24,7 @@
                     <table class="table table-striped table-bordered" id="prosedur-table" data-testid="table-informasi">
                         <thead>
                             <tr>
-                                <th style="max-width: 150px;">Aksi</th>
+                                <th style="max-width: 80px;">Aksi</th>
                                 <th>Judul Prosedur </th>
                             </tr>
                         </thead>
--- resources/views/informasi/sinergi_program/index.blade.php
+++ resources/views/informasi/sinergi_program/index.blade.php
@@ -24,7 +24,7 @@
                     <table class="table table-striped table-bordered" id="sinergi-program-table" data-testid="table-informasi">
                         <thead>
                             <tr>
-                                <th style="max-width: 250px;">Aksi</th>
+                                <th style="max-width: 80px;">Aksi</th>
                                 <th>Nama</th>
                                 <th>URL</th>
                                 <th>Urutan</th>

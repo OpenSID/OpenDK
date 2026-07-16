cd1759c6580f4139f992801db8900a93f831f743
Habibie <habibie.mahbub@gmail.com>
2026-07-16 08:48:40 +0700

    gunakan action-dropdown

--- app/Http/Controllers/Informasi/ProsedurController.php
+++ app/Http/Controllers/Informasi/ProsedurController.php
@@ -98,7 +98,7 @@ class ProsedurController extends Controller
                 $data['download_url'] = auth()->user()->can('access.informasi.prosedur.export') ? route('informasi.prosedur.download', $row->id) : null;
                 $data['preview_url'] = route('informasi.prosedur.preview', $row->id);
 
-                return view('forms.aksi-grup', $data);
+                return view('forms.aksi', $data);
             })
             ->editColumn('judul_prosedur', function ($row) {
                 return $row->judul_prosedur;

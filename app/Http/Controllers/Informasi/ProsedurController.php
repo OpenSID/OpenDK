b5fc09f2bf56b5b630a39f6bc29fe1845b09c560
Habibie <habibie.mahbub@gmail.com>
2026-07-16 08:53:27 +0700

    sesuaikan guard prosedur

--- app/Http/Controllers/Informasi/ProsedurController.php
+++ app/Http/Controllers/Informasi/ProsedurController.php
@@ -96,7 +96,7 @@ class ProsedurController extends Controller
                 }
 
                 $data['download_url'] = auth()->user()->can('access.informasi.prosedur.export') ? route('informasi.prosedur.download', $row->id) : null;
-                $data['preview_url'] = route('informasi.prosedur.preview', $row->id);
+                $data['preview_url'] = auth()->user()->can('access.informasi.prosedur.view') ? route('informasi.prosedur.preview', $row->id) : null;
 
                 return view('forms.aksi', $data);
             })
@@ -209,6 +209,10 @@ class ProsedurController extends Controller
     public function preview(Prosedur $prosedur)
     {
         $path = $prosedur->file_prosedur;
+
+        if (empty($path)) {
+            abort(404, 'File tidak ditemukan.');
+        }
 
         if (file_exists(public_path($path))) {
             return response()->file(public_path($path));
